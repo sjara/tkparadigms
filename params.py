2221426a4f0d3883e6afae70d1de20fc38a82356
Santiago Jaramillo <sjara@uoregon.edu>
2015-05-20 09:45:59 -0700

    Changed adap006-010 to direct mode.

--- params.py
+++ params.py
@@ -102,7 +102,7 @@ cued006 = pardict.copy()
 
 
 # -- Adaptive categorization, psychometric and switching --
-latestMiceMode = sidesDirectMode
+latestMiceMode = directMode
 
 pardict = {'subject':'adap006','experimenter':'santiago'}
 pardict.update(latestMiceMode)
