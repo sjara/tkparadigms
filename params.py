21d6564f4cb7042501a4d76d4f83d7ad93128824
Nick Ponvert <nickponvert@gmail.com>
2016-04-18 11:05:35 -0700

    Took new adap animals off anti-bias mode

--- params.py
+++ params.py
@@ -45,31 +45,31 @@ cuedDirectMode = {'outcomeMode':'direct', 'delayToTargetMean':0, 'delayToTargetH
 # -- New adap animals
 pardict = {'subject':'adap026','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+# pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(basicDiscriminationMode)
 adap026 = pardict.copy()
 
 pardict = {'subject':'adap027','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+# pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(basicDiscriminationMode)
 adap027 = pardict.copy()
 
 pardict = {'subject':'adap028','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+# pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(basicDiscriminationMode)
 adap028 = pardict.copy()
 
 pardict = {'subject':'adap029','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+# pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(basicDiscriminationMode)
 adap029 = pardict.copy()
 
 pardict = {'subject':'adap030','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+# pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(basicDiscriminationMode)
 adap030 = pardict.copy()
 
