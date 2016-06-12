1f16b6167982750ff88259446a2a0171b8678b30
LanGuo <lan.guo14@gmail.com>
2016-06-11 18:59:43 -0700

    removed antibias mode for dipi014 to d1pi017

--- params.py
+++ params.py
@@ -125,25 +125,25 @@ d1pi013 = pardict.copy()
 
 pardict = {'subject':'d1pi014','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+#pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(d1pi1)
 d1pi014 = pardict.copy()
 
 pardict = {'subject':'d1pi015','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+#pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(d1pi1)
 d1pi015 = pardict.copy()
 
 pardict = {'subject':'d1pi016','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+#pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(d1pi1)
 d1pi016 = pardict.copy()
 
 pardict = {'subject':'d1pi017','experimenter':'santiago'}
 pardict.update(frequencySet6to19)
-pardict.update({'antibiasMode':'repeat_mistake'})
+#pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update(d1pi1)
 d1pi017 = pardict.copy()
 
