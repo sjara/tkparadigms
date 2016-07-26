87fd732f871c1f9397e2162c0097b5c3848bf793
Nick Ponvert <nickponvert@gmail.com>
2016-07-26 10:04:35 -0700

    Moved amod6-10 to tones psycurve

--- params.py
+++ params.py
@@ -328,7 +328,7 @@ amodMice_regularTask.update({'punishTimeError':2})
 amodSidesDirect = {'outcomeMode':'sides_direct', 'soundTypeMode':'amp_mod'}
 amodDirect = {'outcomeMode':'direct', 'soundTypeMode':'amp_mod'}
 amodNextCorrectAM = {'outcomeMode':'on_next_correct', 'soundTypeMode':'amp_mod'}
-amodIfCorrectTones = {'outcomeMode':'only_if_correct', 'soundTypeMode':'amp_mod'}
+amodIfCorrectAM = {'outcomeMode':'only_if_correct', 'soundTypeMode':'amp_mod'}
 amodPsycurveAM = {'outcomeMode':'only_if_correct', 'soundTypeMode':'amp_mod', 'psycurveMode':'uniform'}
 amodIfCorrectTones = {'outcomeMode':'only_if_correct', 'soundTypeMode':'tones'}
 amodPsycurveTones = {'outcomeMode':'only_if_correct', 'soundTypeMode':'tones', 'psycurveMode':'uniform'}
@@ -367,33 +367,33 @@ amod004 = pardict.copy()
 ## Starting on direct mode with amp mod only
 
 pardict = {'subject':'amod006','experimenter':'nick'}
-pardict.update(amodIfCorrectTones)
+pardict.update(amodPsycurveTones)
 # pardict.update({'targetIntensityMode':'randMinus20'})
 # pardict.update({'antibiasMode':'repeat_mistake'})
 # pardict.update({'psycurveMode':'uniform'})
 amod006 = pardict.copy()
 
 pardict = {'subject':'amod007','experimenter':'nick'}
-pardict.update(amodIfCorrectTones)
+pardict.update(amodPsycurveTones)
 # pardict.update({'targetIntensityMode':'randMinus20'})
 # pardict.update({'psycurveMode':'uniform'})
 amod007 = pardict.copy()
 
 pardict = {'subject':'amod008','experimenter':'nick'}
-pardict.update(amodIfCorrectTones)
+pardict.update(amodPsycurveTones)
 # pardict.update({'targetIntensityMode':'randMinus20'})
 # pardict.update({'antibiasMode':'repeat_mistake'})
 # pardict.update({'psycurveMode':'uniform'})
 amod008 = pardict.copy()
 
 pardict = {'subject':'amod009','experimenter':'nick'}
-pardict.update(amodIfCorrectTones)
+pardict.update(amodPsycurveTones)
 # pardict.update({'targetIntensityMode':'randMinus20'})
 # pardict.update({'psycurveMode':'uniform'})
 amod009 = pardict.copy()
 
 pardict = {'subject':'amod010','experimenter':'nick'}
-pardict.update(amodIfCorrectTones)
+pardict.update(amodPsycurveTones)
 # pardict.update({'targetIntensityMode':'randMinus20'})
 # pardict.update({'antibiasMode':'repeat_mistake'})
 # pardict.update({'psycurveMode':'uniform'})
