6f3c63aa07dddf0c69f399b1f2cbb1c6cfcd8d7a
JaraUser <jarauser@jarastation3>
2016-09-16 12:33:50 -0700

    Created params file for photoinactivation during task.

---
+++ params.tai.py
@@ -0,0 +1,20 @@
+'''
+Parameters for photoinactivation during freq discrimination task (photostim_freq_discrim.py)
+'''
+
+frequencySet6to19 = {'lowFreq':6200,'midFreq':11000,'highFreq':19200}
+
+psyCurvePhotoInactivation = {'trialsPerBlock':2000,'punishTimeError':4,'delayToTargetMean':0.2,
+                             'currentBlock':'mid_boundary','psycurveMode':'uniform',
+                             'laserFrontOverhang':0.05, 'laserBackOverhang':0.1,
+                             'percentLaserTrialLeft':0.3, 'percentLaserTrialRight':0}
+
+pardict = {'subject':'adap038','experimenter':'tai'}
+pardict.update(frequencySet6to19)
+pardict.update(psyCurvePhotoInactivation)
+adap038 = pardict.copy()
+
+pardict = {'subject':'adap031','experimenter':'tai'}
+pardict.update(frequencySet6to19)
+pardict.update(psyCurvePhotoInactivation)
+adap031 = pardict.copy()
