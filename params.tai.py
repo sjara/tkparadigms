bc65e2e1c2cb43f49f4dc8ad92ad9a346867c6a0
JaraUser <jarauser@jarastation3>
2016-10-10 15:51:06 -0700

    tai's temporary param changes

--- params.tai.py
+++ params.tai.py
@@ -7,7 +7,7 @@ frequencySet6to19 = {'lowFreq':6200,'midFreq':11000,'highFreq':19200}
 psyCurvePhotoInactivation = {'trialsPerBlock':2000,'punishTimeError':4,'delayToTargetMean':0.2,
                              'currentBlock':'mid_boundary','psycurveMode':'uniform',
                              'laserFrontOverhang':0.05, 'laserBackOverhang':0.1,
-                             'percentLaserTrialLeft':0.3, 'percentLaserTrialRight':0}
+                             'percentLaserTrialLeft':0.3, 'percentLaserTrialRight':0, }
 
 pardict = {'subject':'adap038','experimenter':'tai'}
 pardict.update(frequencySet6to19)
@@ -18,3 +18,8 @@ pardict = {'subject':'adap031','experimenter':'tai'}
 pardict.update(frequencySet6to19)
 pardict.update(psyCurvePhotoInactivation)
 adap031 = pardict.copy()
+
+pardict = {'subject':'adap031','experimenter':'tai'}
+pardict.update(frequencySet6to19)
+pardict.update(psyCurvePhotoInactivation)
+adap031_interval = pardict.copy()
