4e1ecff5baa117627201de405150b9b318d86230
JaraUser <jarauser@jarastation3>
2016-10-10 16:37:13 -0700

    Updated params for adap031 in new paradigm photostim_interval_freq_discrim

--- params.tai.py
+++ params.tai.py
@@ -9,17 +9,26 @@ psyCurvePhotoInactivation = {'trialsPerBlock':2000,'punishTimeError':4,'delayToT
                              'laserFrontOverhang':0.05, 'laserBackOverhang':0.1,
                              'percentLaserTrialLeft':0.3, 'percentLaserTrialRight':0, }
 
+#Params for paradigm 'photostim_interval_freq_discrim.py' that Santiago wrote for presenting laser at three different times
+psyCurvePhotoInactivation_interval = {'delayToTargetHalfRange':0.5,'punishTimeError':4,
+					'psycurveMode':'uniform','laserDuration':0.1,
+					'laserOnsetFromSoundOnset1':-0.1,'laserOnsetFromSoundOnset2':0,
+					'laserOnsetFromSoundOnset3':0.1,'nOnsetsToUse':'3',
+					'percentTrialsEachLaserMode':0.25}
+
 pardict = {'subject':'adap038','experimenter':'tai'}
 pardict.update(frequencySet6to19)
 pardict.update(psyCurvePhotoInactivation)
 adap038 = pardict.copy()
 
+#params for adap031 in paradigm 'photostim_freq_discrim.py'
 pardict = {'subject':'adap031','experimenter':'tai'}
 pardict.update(frequencySet6to19)
 pardict.update(psyCurvePhotoInactivation)
 adap031 = pardict.copy()
 
+#params for adap031 in paradigm 'photostim_interval_freq_discrim.py'
 pardict = {'subject':'adap031','experimenter':'tai'}
 pardict.update(frequencySet6to19)
-pardict.update(psyCurvePhotoInactivation)
+pardict.update(psyCurvePhotoInactivation_interval)
 adap031_interval = pardict.copy()
