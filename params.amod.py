d726a32b165acdbe0538f7bc24cbba49fce1fdca
Nick Ponvert <nickponvert@gmail.com>
2016-11-28 09:09:08 -0800

    Put amod 12-14 on anti bias mode

--- params.amod.py
+++ params.amod.py
@@ -7,23 +7,23 @@ amodIfCorrectTones = {'outcomeMode':'only_if_correct', 'soundTypeMode':'tones'}
 amodPsycurveTones = {'outcomeMode':'only_if_correct', 'soundTypeMode':'tones', 'psycurveMode':'uniform'}
 amodPsycurveMixed = {'outcomeMode':'only_if_correct', 'soundTypeMode':'mixed_tones', 'psycurveMode':'uniform'}
 
-#Current training state for the new batch of amod mice
-currentState1 = amodNextCorrectAM
-currentState2 = amodIfCorrectAM
 
 pardict = {'subject':'amod011','experimenter':'nick'}
-pardict.update(currentState2)
+pardict.update(amodIfCorrectAM)
 amod011 = pardict.copy()
 
 pardict = {'subject':'amod012','experimenter':'nick'}
-pardict.update(currentState1)
+pardict.update(amodIfCorrectAM)
+pardict.update({'antibiasMode':'repeat_mistake'})
 amod012 = pardict.copy()
 
 pardict = {'subject':'amod013','experimenter':'nick'}
-pardict.update(currentState2)
+pardict.update(amodIfCorrectAM)
+pardict.update({'antibiasMode':'repeat_mistake'})
 amod013 = pardict.copy()
 
 pardict = {'subject':'amod014','experimenter':'nick'}
-pardict.update(currentState2)
+pardict.update(amodIfCorrectAM)
+pardict.update({'antibiasMode':'repeat_mistake'})
 amod014 = pardict.copy()
 
