fb7d0659c4e2aa7d9151656f55fdec9efc8fa41a
Nick Ponvert <nickponvert@gmail.com>
2017-02-14 13:34:41 -0800

    Moved amod animals to if correct tones mode

--- params.amod.py
+++ params.amod.py
@@ -9,21 +9,25 @@ amodPsycurveMixed = {'outcomeMode':'only_if_correct', 'soundTypeMode':'mixed_ton
 
 
 pardict = {'subject':'amod011','experimenter':'nick'}
-pardict.update(amodPsycurveAM)
+# pardict.update(amodPsycurveAM)
+pardict.update(amodIfCorrectTones)
 amod011 = pardict.copy()
 
 pardict = {'subject':'amod012','experimenter':'nick'}
-pardict.update(amodPsycurveAM)
+# pardict.update(amodPsycurveAM)
+pardict.update(amodIfCorrectTones)
 # pardict.update({'antibiasMode':'repeat_mistake'})
 amod012 = pardict.copy()
 
 pardict = {'subject':'amod013','experimenter':'nick'}
-pardict.update(amodPsycurveAM)
+# pardict.update(amodPsycurveAM)
+pardict.update(amodIfCorrectTones)
 # pardict.update({'antibiasMode':'repeat_mistake'})
 amod013 = pardict.copy()
 
 pardict = {'subject':'amod014','experimenter':'nick'}
-pardict.update(amodPsycurveAM)
+# pardict.update(amodPsycurveAM)
+pardict.update(amodIfCorrectTones)
 # pardict.update({'antibiasMode':'repeat_mistake'})
 amod014 = pardict.copy()
 
