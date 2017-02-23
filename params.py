071eb5d7af150aa4231bdbde21c3f465d0aa9240
Anna Lakunina <alakunina@gmail.com>
2017-02-23 09:44:56 -0800

    testing unimplanted band mice at different noise amps

--- params.py
+++ params.py
@@ -281,12 +281,14 @@ band017 = pardict.copy()
 
 pardict = {'subject': 'band018', 'experimenter': 'anna'}
 pardict.update(bandSNR)
-pardict.update(oneNoiseThreshMode)
+pardict.update(threeNoiseThreshMode)
+pardict.update({'numAmps':2})
 band018 = pardict.copy()
 
 pardict = {'subject': 'band019', 'experimenter': 'anna'}
 pardict.update(bandSNR)
-pardict.update(oneNoiseThreshMode)
+pardict.update(threeNoiseThreshMode)
+pardict.update({'numAmps':2})
 pardict.update({'minSNR':-10, 'maxSNR':10})
 #pardict.update({'antibiasMode':'repeat_mistake'})
 band019 = pardict.copy()
