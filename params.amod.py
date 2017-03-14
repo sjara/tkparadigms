65e6a09788b9dd6b8132a4aa78335bea2ec72da7
Nick Ponvert <nickponvert@gmail.com>
2017-03-14 14:05:44 -0700

    Amod011 set punish time error to 4, other amods to mixed mode

--- params.amod.py
+++ params.amod.py
@@ -5,30 +5,31 @@ amodIfCorrectAM = {'outcomeMode':'only_if_correct', 'soundTypeMode':'amp_mod'}
 amodPsycurveAM = {'outcomeMode':'only_if_correct', 'soundTypeMode':'amp_mod', 'psycurveMode':'uniform'}
 amodIfCorrectTones = {'outcomeMode':'only_if_correct', 'soundTypeMode':'tones'}
 amodPsycurveTones = {'outcomeMode':'only_if_correct', 'soundTypeMode':'tones', 'psycurveMode':'uniform'}
+amodIfCorrectMixed = {'outcomeMode':'only_if_correct', 'soundTypeMode':'mixed_tones'}
 amodPsycurveMixed = {'outcomeMode':'only_if_correct', 'soundTypeMode':'mixed_tones', 'psycurveMode':'uniform'}
 
 
 pardict = {'subject':'amod011','experimenter':'nick'}
 # pardict.update(amodPsycurveAM)
 pardict.update(amodIfCorrectTones)
-# pardict.update({'antibiasMode':'repeat_mistake'})
+pardict.update({'punishTimeError':4})
 amod011 = pardict.copy()
 
 pardict = {'subject':'amod012','experimenter':'nick'}
-pardict.update(amodPsycurveTones)
-# pardict.update(amodIfCorrectTones)
+# pardict.update(amodPsycurveMixed)
+pardict.update(amodIfCorrectMixed)
 # pardict.update({'antibiasMode':'repeat_mistake'})
 amod012 = pardict.copy()
 
 pardict = {'subject':'amod013','experimenter':'nick'}
-pardict.update(amodPsycurveTones)
-# pardict.update(amodIfCorrectTones)
+# pardict.update(amodPsycurveMixed)
+pardict.update(amodIfCorrectMixed)
 # pardict.update({'antibiasMode':'repeat_mistake'})
 amod013 = pardict.copy()
 
 pardict = {'subject':'amod014','experimenter':'nick'}
-pardict.update(amodPsycurveTones)
-# pardict.update(amodIfCorrectTones)
+# pardict.update(amodPsycurveMixed)
+pardict.update(amodIfCorrectMixed)
 # pardict.update({'antibiasMode':'repeat_mistake'})
 amod014 = pardict.copy()
 
