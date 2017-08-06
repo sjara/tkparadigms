5fbc7e63d0e336290930dbaa243cceb82b191a7a
Santiago Jaramillo <sjara@users.noreply.github.com>
2017-08-06 01:44:11 -0700

    Changed new mice to direct mode.

--- params.halli.py
+++ params.halli.py
@@ -21,60 +21,60 @@ basicDiscriminationMode = {'delayToTargetMean':0.2, 'currentBlock':'mid_boundary
 
 pardict = {'subject':'adap062','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap062 = pardict.copy()
 
 pardict = {'subject':'adap063','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap063 = pardict.copy()
 
 pardict = {'subject':'adap064','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap064 = pardict.copy()
 
 pardict = {'subject':'adap065','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap065 = pardict.copy()
 
 pardict = {'subject':'adap066','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap066 = pardict.copy()
 
 pardict = {'subject':'adap067','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(directMode)
+pardict.update(directMode) # Change to next stage
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap067 = pardict.copy()
 
 pardict = {'subject':'adap068','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap068 = pardict.copy()
 
 pardict = {'subject':'adap069','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap069 = pardict.copy()
 
 pardict = {'subject':'adap070','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap070 = pardict.copy()
 
 pardict = {'subject':'adap071','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(sidesDirectMode)
+pardict.update(directMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap071 = pardict.copy()
