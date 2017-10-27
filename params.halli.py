481ebce7becb884f46cdbbe0c7c51c528f32af6e
Hallir <30684519+Hallir@users.noreply.github.com>
2017-10-27 00:36:18 -0700

    77, 80 off antibias. 79 on antibias.

--- params.halli.py
+++ params.halli.py
@@ -155,7 +155,7 @@ adap076 = pardict.copy()
 pardict = {'subject':'adap077','experimenter':'halli'}
 pardict.update(frequencySet6to19)
 pardict.update(M3requireCorrectMode)
-pardict.update({'antibiasMode':'repeat_mistake'})
+#pardict.update({'antibiasMode':'repeat_mistake'})
 adap077 = pardict.copy()
 
 pardict = {'subject':'adap078','experimenter':'halli'}
@@ -167,13 +167,13 @@ adap078 = pardict.copy()
 pardict = {'subject':'adap079','experimenter':'halli'}
 pardict.update(frequencySet6to19)
 pardict.update(M3requireCorrectMode)
-#pardict.update({'antibiasMode':'repeat_mistake'})
+pardict.update({'antibiasMode':'repeat_mistake'})
 adap079 = pardict.copy()
 
 pardict = {'subject':'adap080','experimenter':'halli'}
 pardict.update(frequencySet6to19)
 pardict.update(M3requireCorrectMode)
-pardict.update({'antibiasMode':'repeat_mistake'})
+#pardict.update({'antibiasMode':'repeat_mistake'})
 adap080 = pardict.copy()
 
 pardict = {'subject':'adap081','experimenter':'halli'}
