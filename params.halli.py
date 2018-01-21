9a782fdd41df7ab40d76a060ebab24cdf8b30a59
Hallir <30684519+Hallir@users.noreply.github.com>
2018-01-20 18:10:45 -0800

    Update params.halli.py

--- params.halli.py
+++ params.halli.py
@@ -124,7 +124,7 @@ adap071 = pardict.copy()
 
 pardict = {'subject':'adap072','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(M2basicDiscriminationMode)
+pardict.update(M2psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap072 = pardict.copy()
 
@@ -148,7 +148,7 @@ adap075 = pardict.copy()
 
 pardict = {'subject':'adap076','experimenter':'halli'}
 pardict.update(frequencySet6to19)
-pardict.update(psyCurveMode)
+pardict.update(M2psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 adap076 = pardict.copy()
 
