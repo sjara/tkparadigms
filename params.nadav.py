abe43aeecb16d8c5996999e261fc5a66be85fd61
Anna Lakunina <alakunina@gmail.com>
2018-01-29 10:10:09 -0800

    nadav's params

---
+++ params.nadav.py
@@ -0,0 +1,42 @@
+# -- dictionaries of params for different stages --
+
+sidesDirectMode = {'outcomeMode':'sides_direct', 'threshMode': 'max_only', 'maxSNR': 20, 'bandMode': 'white_only'}
+
+directMode = {'outcomeMode':'direct', 'threshMode': 'max_only', 'maxSNR': 20, 'bandMode': 'white_only'}
+
+nextCorrectNoDel = {'outcomeMode':'on_next_correct', 'threshMode':'max_only', 'maxSNR':20, 'bandMode':'white_only' }
+
+
+# -- bandwidth mice --
+
+pardict = {'subject': 'band046', 'experimenter': 'nadav'}
+pardict.update(sidesDirectMode)
+band006 = pardict.copy()
+
+pardict = {'subject': 'band047', 'experimenter': 'nadav'}
+pardict.update(sidesDirectMode)
+band006 = pardict.copy()
+
+pardict = {'subject': 'band048', 'experimenter': 'nadav'}
+pardict.update(sidesDirectMode)
+band006 = pardict.copy()
+
+pardict = {'subject': 'band049', 'experimenter': 'nadav'}
+pardict.update(sidesDirectMode)
+band006 = pardict.copy()
+
+pardict = {'subject': 'band050', 'experimenter': 'nadav'}
+pardict.update(sidesDirectMode)
+band006 = pardict.copy()
+
+pardict = {'subject': 'band051', 'experimenter': 'nadav'}
+pardict.update(sidesDirectMode)
+band006 = pardict.copy()
+
+pardict = {'subject': 'band052', 'experimenter': 'nadav'}
+pardict.update(sidesDirectMode)
+band006 = pardict.copy()
+
+pardict = {'subject': 'band053', 'experimenter': 'nadav'}
+pardict.update(sidesDirectMode)
+band006 = pardict.copy()
