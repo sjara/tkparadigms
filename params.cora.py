fc50966eb1b4936d41aa855a0b5a5dbcd725a218
Santiago Jaramillo <sjara@users.noreply.github.com>
2018-06-29 16:29:18 -0700

    Update bili mice.

--- params.cora.py
+++ params.cora.py
@@ -13,19 +13,19 @@ increaseDelayMode = {'outcomeMode':'on_next_correct', 'delayToTargetMean':0, 'de
 
 pardict = {'subject':'bili001','experimenter':'cora'}
 pardict.update({'relevantFeature':'spectral'})
-pardict.update(directMode)
+pardict.update(increaseDelayMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili001 = pardict.copy()
 
 pardict = {'subject':'bili002','experimenter':'cora'}
 pardict.update({'relevantFeature':'temporal'})
-pardict.update(directMode)
+pardict.update(increaseDelayMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili002 = pardict.copy()
 
 pardict = {'subject':'bili003','experimenter':'cora'}
 pardict.update({'relevantFeature':'spectral'})
-pardict.update(directMode)
+pardict.update(increaseDelayMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili003 = pardict.copy()
 
@@ -37,19 +37,19 @@ bili004 = pardict.copy()
 
 pardict = {'subject':'bili005','experimenter':'cora'}
 pardict.update({'relevantFeature':'spectral'})
-pardict.update(directMode)
+pardict.update(increaseDelayMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili005 = pardict.copy()
 
 pardict = {'subject':'bili006','experimenter':'cora'}
 pardict.update({'relevantFeature':'temporal'})
-pardict.update(directMode)
+pardict.update(increaseDelayMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili006 = pardict.copy()
 
 pardict = {'subject':'bili007','experimenter':'cora'}
 pardict.update({'relevantFeature':'spectral'})
-pardict.update(directMode)
+pardict.update(increaseDelayMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili007 = pardict.copy()
 
