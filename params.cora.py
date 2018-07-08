38b220b28571e5f74aafebab7c63361b48e268df
cblossomp <40777587+cblossomp@users.noreply.github.com>
2018-07-08 12:45:04 -0700

    bili005 & bili007: removed antibias. bili001 & bili002: added antibias

--- params.cora.py
+++ params.cora.py
@@ -15,13 +15,13 @@ requireCorrectMode = {'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2, 
 pardict = {'subject':'bili001','experimenter':'cora'}
 pardict.update({'relevantFeature':'spectral'})
 pardict.update(requireCorrectMode)
-#pardict.update({'antibiasMode':'repeat_mistake'})
+pardict.update({'antibiasMode':'repeat_mistake'})
 bili001 = pardict.copy()
 
 pardict = {'subject':'bili002','experimenter':'cora'}
 pardict.update({'relevantFeature':'temporal'})
 pardict.update(requireCorrectMode)
-#pardict.update({'antibiasMode':'repeat_mistake'})
+pardict.update({'antibiasMode':'repeat_mistake'})
 bili002 = pardict.copy()
 
 pardict = {'subject':'bili003','experimenter':'cora'}
@@ -39,7 +39,7 @@ bili004 = pardict.copy()
 pardict = {'subject':'bili005','experimenter':'cora'}
 pardict.update({'relevantFeature':'spectral'})
 pardict.update(requireCorrectMode)
-pardict.update({'antibiasMode':'repeat_mistake'})
+#pardict.update({'antibiasMode':'repeat_mistake'})
 bili005 = pardict.copy()
 
 pardict = {'subject':'bili006','experimenter':'cora'}
@@ -51,7 +51,7 @@ bili006 = pardict.copy()
 pardict = {'subject':'bili007','experimenter':'cora'}
 pardict.update({'relevantFeature':'spectral'})
 pardict.update(requireCorrectMode)
-pardict.update({'antibiasMode':'repeat_mistake'})
+#pardict.update({'antibiasMode':'repeat_mistake'})
 bili007 = pardict.copy()
 
 
