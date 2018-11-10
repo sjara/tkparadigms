e1c6063beec7a08872fe929d5bb3599a66b2fcbf
Santiago Jaramillo <sjara@users.noreply.github.com>
2018-11-09 16:27:17 -0800

    Changed all mice to the opposite relevant feature.

--- params.cora.py
+++ params.cora.py
@@ -15,45 +15,52 @@ psyCurveMode = {'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2, 'delay
 # ======== Parameters for each animal =========
 
 pardict = {'subject':'bili001','experimenter':'cora'}
-pardict.update({'relevantFeature':'spectral'})
+#pardict.update({'relevantFeature':'spectral'})
+pardict.update({'relevantFeature':'temporal'})
 #pardict.update(requireCorrectMode)
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili001 = pardict.copy()
 
 pardict = {'subject':'bili002','experimenter':'cora'}
-pardict.update({'relevantFeature':'temporal'})
+#pardict.update({'relevantFeature':'temporal'})
+pardict.update({'relevantFeature':'spectral'})
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili002 = pardict.copy()
 
 pardict = {'subject':'bili003','experimenter':'cora'}
-pardict.update({'relevantFeature':'spectral'})
+#pardict.update({'relevantFeature':'spectral'})
+pardict.update({'relevantFeature':'temporal'})
 #pardict.update(requireCorrectMode)
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili003 = pardict.copy()
 
 pardict = {'subject':'bili004','experimenter':'cora'}
-pardict.update({'relevantFeature':'temporal'})
+#pardict.update({'relevantFeature':'temporal'})
+pardict.update({'relevantFeature':'spectral'})
 pardict.update(psyCurveMode)
 pardict.update({'antibiasMode':'repeat_mistake'})
 bili004 = pardict.copy()
 
 pardict = {'subject':'bili005','experimenter':'cora'}
-pardict.update({'relevantFeature':'spectral'})
+#pardict.update({'relevantFeature':'spectral'})
+pardict.update({'relevantFeature':'temporal'})
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili005 = pardict.copy()
 
 pardict = {'subject':'bili006','experimenter':'cora'}
-pardict.update({'relevantFeature':'temporal'})
+#pardict.update({'relevantFeature':'temporal'})
+pardict.update({'relevantFeature':'spectral'})
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili006 = pardict.copy()
 
 pardict = {'subject':'bili007','experimenter':'cora'}
-pardict.update({'relevantFeature':'spectral'})
+#pardict.update({'relevantFeature':'spectral'})
+pardict.update({'relevantFeature':'temporal'})
 #pardict.update(requireCorrectMode)
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
