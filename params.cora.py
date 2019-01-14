03960a03e6fc1434170609b7178dd68a73317cb5
Santiago Jaramillo <sjara@users.noreply.github.com>
2019-01-14 15:03:45 -0800

    Changed relevant feature of many animals.
    I changed those animals that were not performing well back to the original feature they were trained in (either spectral or temporal). Only bili001 and bili002 switched well and were left in the new feature.

--- params.cora.py
+++ params.cora.py
@@ -30,37 +30,37 @@ pardict.update(psyCurveMode)
 bili002 = pardict.copy()
 
 pardict = {'subject':'bili003','experimenter':'cora'}
-#pardict.update({'relevantFeature':'spectral'})
-pardict.update({'relevantFeature':'temporal'})
+pardict.update({'relevantFeature':'spectral'})
+#pardict.update({'relevantFeature':'temporal'})
 #pardict.update(requireCorrectMode)
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili003 = pardict.copy()
 
 pardict = {'subject':'bili004','experimenter':'cora'}
-#pardict.update({'relevantFeature':'temporal'})
-pardict.update({'relevantFeature':'spectral'})
+pardict.update({'relevantFeature':'temporal'})
+#pardict.update({'relevantFeature':'spectral'})
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili004 = pardict.copy()
 
 pardict = {'subject':'bili005','experimenter':'cora'}
-#pardict.update({'relevantFeature':'spectral'})
-pardict.update({'relevantFeature':'temporal'})
+pardict.update({'relevantFeature':'spectral'})
+#pardict.update({'relevantFeature':'temporal'})
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili005 = pardict.copy()
 
 pardict = {'subject':'bili006','experimenter':'cora'}
-#pardict.update({'relevantFeature':'temporal'})
-pardict.update({'relevantFeature':'spectral'})
+pardict.update({'relevantFeature':'temporal'})
+#pardict.update({'relevantFeature':'spectral'})
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili006 = pardict.copy()
 
 pardict = {'subject':'bili007','experimenter':'cora'}
-#pardict.update({'relevantFeature':'spectral'})
-pardict.update({'relevantFeature':'temporal'})
+pardict.update({'relevantFeature':'spectral'})
+#pardict.update({'relevantFeature':'temporal'})
 #pardict.update(requireCorrectMode)
 pardict.update(psyCurveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
