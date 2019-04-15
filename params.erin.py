3ec0c9f6e1a05ddb4156305280a328b4d46620d9
epetruccione15 <49211648+epetruccione15@users.noreply.github.com>
2019-04-15 14:31:09 -0700

    bili029 on AB

--- params.erin.py
+++ params.erin.py
@@ -84,7 +84,7 @@ bili028 = pardict.copy()
 
 pardict = {'subject':'bili029','experimenter':'erin'}
 pardict.update(requireCorrectMode)
-#pardict.update({'antibiasMode':'repeat_mistake'})
+pardict.update({'antibiasMode':'repeat_mistake'})
 bili029 = pardict.copy()
 
 pardict = {'subject':'bili030','experimenter':'erin'}
