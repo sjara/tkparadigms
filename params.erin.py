fec9e202debbbd9441504278131a4a3ea3853d0c
epetruccione15 <49211648+epetruccione15@users.noreply.github.com>
2019-04-29 13:13:49 -0700

    bili016 passive for 4-30

--- params.erin.py
+++ params.erin.py
@@ -27,7 +27,7 @@ bili000 = pardict.copy()
 ###bili016-024 (PV-ChR2)
 
 pardict = {'subject':'bili016','experimenter':'erin'}
-pardict.update(activeMode)
+pardict.update(passiveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili016 = pardict.copy()
 
