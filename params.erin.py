c1d520fedf6eee0e6bd847dbdb83558c6a02686c
epetruccione15 <49211648+epetruccione15@users.noreply.github.com>
2019-05-08 13:06:00 -0700

    31 active, 32 33 RC, 25-27 extreme passive

--- params.erin.py
+++ params.erin.py
@@ -76,17 +76,17 @@ bili024 = pardict.copy()
 # -----
 
 pardict = {'subject':'bili025','experimenter':'erin'}
-pardict.update(requireCorrectMode)
+pardict.update(passiveModeExtremes)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili025 = pardict.copy()
 
 pardict = {'subject':'bili026','experimenter':'erin'}
-pardict.update(requireCorrectMode)
+pardict.update(passiveModeExtremes)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili026 = pardict.copy()
 
 pardict = {'subject':'bili027','experimenter':'erin'}
-pardict.update(requireCorrectMode)
+pardict.update(passiveModeExtremes)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili027 = pardict.copy()
 
@@ -106,17 +106,17 @@ pardict.update(requireCorrectMode)
 bili030 = pardict.copy()
 
 pardict = {'subject':'bili031','experimenter':'erin'}
-pardict.update(passiveMode)
+pardict.update(activeMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili031 = pardict.copy()
 
 pardict = {'subject':'bili032','experimenter':'erin'}
-pardict.update(passiveModeExtremes)
+pardict.update(requireCorrectMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili032 = pardict.copy()
 
 pardict = {'subject':'bili033','experimenter':'erin'}
-pardict.update(passiveModeExtremes)
+pardict.update(requireCorrectMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili033 = pardict.copy()
 
