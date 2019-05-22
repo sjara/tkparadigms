06f13546c65e14540bdaf47a9c32d73fb7546f33
epetruccione15 <49211648+epetruccione15@users.noreply.github.com>
2019-05-21 22:00:21 -0700

    27 31 32 passive, 29 active

--- params.erin.py
+++ params.erin.py
@@ -86,7 +86,7 @@ pardict.update(activeMode)
 bili026 = pardict.copy()
 
 pardict = {'subject':'bili027','experimenter':'erin'}
-pardict.update(activeMode)
+pardict.update(passiveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili027 = pardict.copy()
 
@@ -96,7 +96,7 @@ pardict.update(activeMode)
 bili028 = pardict.copy()
 
 pardict = {'subject':'bili029','experimenter':'erin'}
-pardict.update(passiveMode)
+pardict.update(activeMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili029 = pardict.copy()
 
@@ -106,12 +106,12 @@ pardict.update(requireCorrectMode)
 bili030 = pardict.copy()
 
 pardict = {'subject':'bili031','experimenter':'erin'}
-pardict.update(activeMode)
+pardict.update(passiveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili031 = pardict.copy()
 
 pardict = {'subject':'bili032','experimenter':'erin'}
-pardict.update(activeMode)
+pardict.update(passiveMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 bili032 = pardict.copy()
 
