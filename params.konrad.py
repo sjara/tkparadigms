0edcb9b8fc945415549747518a65ffdf78771d70
Jara User <jarauser@jararig1>
2019-07-01 17:46:50 -0700

    Added oddball params

--- params.konrad.py
+++ params.konrad.py
@@ -1,34 +1,75 @@
-'''
-Define parameters for different subjects
-'''
+subject = 'chad'
 
-basicParams = {'trainer':'kb', 'stimPreDuration':1}
+noiseburst = {'stimDur':0.1,
+               'isiMean':0.9,
+               'isiHalfRange': 0.01,
+               'stimType' : 'Noise',
+               'minInt':60,
+               'maxInt':60,
+               'numInt':1,
+               'subject':subject,
+               'experimenter' : 'konrad'}
+
+rlf = {'stimDur':0.1,
+       'isiMean':0.9,
+       'isiHalfRange': 0.01,
+       'stimType' : 'Noise',
+       'minInt':30,
+       'maxInt':70,
+       'numInt':9,
+       'subject':subject,
+       'experimenter' : 'konrad'}
+
+lasertrain = {'stimDur':0.01,
+               'isiMean':1,
+               'isiHalfRange': 0,
+               'stimType' : 'LaserTrain',
+               'subject':subject,
+               'experimenter' : 'konrad'}
 
 
+laserpulse = {'stimDur':0.1,
+               'isiMean':0.9,
+               'isiHalfRange': 0,
+               'stimType' : 'Laser',
+               'subject':subject,
+               'experimenter' : 'konrad'}
 
-# ======== Parameters for each animal =========
+tc = {'stimDur':0.1,
+      'isiMean':0.8,
+      'isiHalfRange': 0.1,
+      'stimType' : 'Sine',
+      'minFreq':2000,
+      'maxFreq':40000,
+      'numTones':16,
+      'minInt':70,
+      'maxInt':70,
+      'numInt':1,
+      'subject':subject,
+      'experimenter' : 'konrad'}
 
-pardict = {'subject':'test000','experimenter':'konrad'}
-pardict.update(basicParams)
-pardict.update({'freq1':3000, 'timeOut':0.1, 'interTrialInterval':0.1})
-test000 = pardict.copy()
+am = {'subject':subject,
+      'experimenter':'konrad',
+      'minFreq':4,
+      'maxFreq':128,
+      'numTones':11,
+      'minInt':60,
+      'maxInt':60,
+      'numInt':1,
+      'stimType':'AM',
+      'stimDur':0.5,
+      'isiMean':1,
+      'isiHalfRange':0.1}
 
-pardict = {'subject':'chad001','experimenter':'konrad'}
-pardict.update(basicParams)
-chad001 = pardict.copy()
-
-pardict = {'subject':'chad002','experimenter':'konrad'}
-pardict.update(basicParams)
-chad002 = pardict.copy()
-
-pardict = {'subject':'chad003','experimenter':'konrad'}
-pardict.update(basicParams)
-chad003 = pardict.copy()
-
-pardict = {'subject':'chad004','experimenter':'konrad'}
-pardict.update(basicParams)
-chad004 = pardict.copy()
-
-pardict = {'subject':'chad005','experimenter':'konrad'}
-pardict.update(basicParams)
-chad005 = pardict.copy()
+oddball = {'subject':subject,
+      'experimenter':'konrad',
+      'standardFreq':0,
+      'oddballFreq':0,
+      'oddballProb':0.1,
+      'soundIntensity':0,
+      'soundDuration':0.1,
+      'isiMean':0.2,
+      'isiHalfRange':0,
+      'stimDuration':0.1,
+      'sequenceMode':'Oddball',
+      'stimType':'Sine'}
