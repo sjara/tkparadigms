0c65a2a4e93be2eefe8236b25abf1c3bf9b1ccd7
Anna Lakunina <alakunina@gmail.com>
2019-08-19 14:51:15 -0700

    updated band params

--- params.py
+++ params.py
@@ -482,12 +482,12 @@ pardict.update(bandOnlyifCorrectOffOnWithdrawal)
 band089 = pardict.copy()
 
 pardict = {'subject': 'band090', 'experimenter': 'anna'}
-pardict.update(bandOnlyifCorrectOffOnWithdrawal)
+pardict.update(bandOnlyCorrectIntMode)
 #pardict.update({'antibiasMode':'repeat_mistake'})
 band090 = pardict.copy()
 
 pardict = {'subject': 'band091', 'experimenter': 'anna'}
-pardict.update(bandOnlyifCorrectOffOnWithdrawal)
+pardict.update(bandOnlyCorrectIntMode)
 band091 = pardict.copy()
 
 pardict = {'subject': 'band092', 'experimenter': 'anna'}
