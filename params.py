33e2bf1563c3b56ebff71c7514343889dc8da2dd
alakunina <alakunina@gmail.com>
2020-06-06 14:21:47 -0700

    Update params.py

--- params.py
+++ params.py
@@ -398,13 +398,13 @@ pardict = {'subject': 'band110', 'experimenter': 'anna'}
 pardict.update(bandEasySNR2BW)
 pardict.update(bandBilateralLaser)
 #pardict.update({'antibiasMode':'repeat_mistake'})
-pardict.update({'threshMode':'max_only'})
+#pardict.update({'threshMode':'max_only'})
 band110 = pardict.copy()
 
 pardict = {'subject': 'band111', 'experimenter': 'anna'}
 pardict.update(bandEasySNR2BW)
 pardict.update(bandBilateralLaser)
-#pardict.update({'antibiasMode':'repeat_mistake'})
+pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update({'threshMode':'max_only'})
 band111 = pardict.copy()
 
@@ -412,7 +412,7 @@ pardict = {'subject': 'band112', 'experimenter': 'anna'}
 pardict.update(bandEasySNR2BW)
 pardict.update(bandBilateralLaser)
 #pardict.update({'antibiasMode':'repeat_mistake'})
-pardict.update({'threshMode':'max_only'})
+#pardict.update({'threshMode':'max_only'})
 band112 = pardict.copy()
 
 
@@ -465,7 +465,7 @@ pardict.update({'toneSide': 'left'})
 band121 = pardict.copy()
 
 pardict = {'subject': 'band122', 'experimenter': 'anna'}
-pardict.update(bandOnlyCorrectIntMode)
+pardict.update(bandOnlyCorrectHardMode)
 pardict.update({'toneSide': 'left'})
 #pardict.update({'antibiasMode':'repeat_mistake'})
 band122 = pardict.copy()
@@ -477,7 +477,7 @@ pardict.update({'antibiasMode':'repeat_mistake'})
 band123 = pardict.copy()
 
 pardict = {'subject': 'band124', 'experimenter': 'anna'}
-pardict.update(bandOnlyCorrectIntMode)
+pardict.update(bandOnlyCorrectHardMode)
 pardict.update({'toneSide': 'left'})
 #pardict.update({'antibiasMode':'repeat_mistake'})
 band124 = pardict.copy()
