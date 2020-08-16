23bd2beb6f2ed563057820a5b2cf51e8f7810cc5
alakunina <alakunina@gmail.com>
2020-08-16 14:32:48 -0700

    Update params.py

--- params.py
+++ params.py
@@ -473,7 +473,7 @@ band120 = pardict.copy()
 pardict = {'subject': 'band127', 'experimenter': 'anna'}
 pardict.update(bandEasySNR2BW)
 pardict.update({'toneSide': 'left'})
-#pardict.update({'antibiasMode':'repeat_mistake'})
+pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update({'threshMode':'max_only'})
 band127 = pardict.copy()
 
@@ -498,7 +498,7 @@ band130 = pardict.copy()
 pardict = {'subject': 'band131', 'experimenter': 'anna'}
 pardict.update(bandEasySNR2BW)
 pardict.update({'toneSide': 'right'})
-#pardict.update({'antibiasMode':'repeat_mistake'})
+pardict.update({'antibiasMode':'repeat_mistake'})
 pardict.update({'threshMode':'max_only'})
 band131 = pardict.copy()
 
