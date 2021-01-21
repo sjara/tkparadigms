db68d074e2382c7c60414c0107a7debbdf10be1d
isalinas4 <isalinas@uoregon.edu>
2021-01-21 15:01:01 -0800

    isabella's params

---
+++ params.isabella.py
@@ -0,0 +1,37 @@
+subject = 'pals001'
+experimenter = 'Isabella'
+
+noiseburst = {'stimDur':0.1,
+               'isiMean':0.9,
+               'isiHalfRange': 0.01,
+               'stimType' : 'Noise',
+               'minInt':60,
+               'maxInt':60,
+               'numInt':1,
+               'subject':subject,
+               'experimenter' : experimenter}
+
+tuningTest = {'stimDur':0.1,
+               'minInt':60,
+               'maxInt':60,
+               'numInt':1,
+               'isiMean':0.6,
+               'isiHalfRange':0.1,
+               'stimType' : 'Sine',
+               'subject':subject,
+               'experimenter' : experimenter}
+
+am = {'subject':subject,
+      'experimenter':experimenter,
+      'minFreq':4,
+      'maxFreq':128,
+      'numTones':11,
+      'minInt':60,
+      'maxInt':60,
+      'numInt':1,
+      'stimType':'AM',
+      'stimDur':0.5,
+      'isiMean':1,
+      'isiHalfRange':0.1}
+      
+
