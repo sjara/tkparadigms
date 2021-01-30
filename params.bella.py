5bdf1f98574cee99e6c3a2e9a6d62fea65bb9732
isalinas4 <isalinas@uoregon.edu>
2021-01-30 14:02:56 -0800

    Bella params for chad055-059

---
+++ params.bella.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Params for mice chad055-059 
+@author: isabe
+"""
+
+
+chad055 = {'psycurveMode':'mid_and_extreme',
+               'psycurveNsteps':8,
+               'taskMode':'discriminate_stim',
+               'stimType' : 'sound_only',
+               'soundType':'chords',
+               'subject':'chad055',
+               'experimenter' : 'isabella'}
+
+chad056 = {'psycurveMode':'mid_and_extreme',
+               'psycurveNsteps':10,
+               'taskMode':'discriminate_stim',
+               'stimType' : 'sound_only',
+               'soundType':'chords',
+               'subject':'chad055',
+               'experimenter' : 'isabella'}
+
+chad057 = {'psycurveMode':'mid_and_extreme',
+               'psycurveNsteps':2,
+               'taskMode':'discriminate_stim',
+               'stimType' : 'sound_only',
+               'soundType':'chords',
+               'subject':'chad055',
+               'experimenter' : 'isabella'}
+
+chad058 = {'psycurveMode':'mid_and_extreme',
+               'psycurveNsteps':8,
+               'taskMode':'discriminate_stim',
+               'stimType' : 'sound_only',
+               'soundType':'chords',
+               'subject':'chad055',
+               'experimenter' : 'isabella'}
+
+chad059 = {'psycurveMode':'mid_and_extreme',
+               'psycurveNsteps':8,
+               'taskMode':'discriminate_stim',
+               'stimType' : 'sound_only',
+               'soundType':'chords',
+               'subject':'chad055',
+               'experimenter' : 'isabella'}
+
