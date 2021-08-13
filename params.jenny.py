765342d92c9349890f4de3310a303f3c300d1c62
isalinas4 <isalinas@uoregon.edu>
2021-08-13 14:05:03 -0700

    fixed typo in params.jenny

--- params.jenny.py
+++ params.jenny.py
@@ -1,5 +1,5 @@
 tuningFreq = {'subject':'pals023','experimenter':'jenny', 'minFreq':2000, 'maxFreq':40000,
-              'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1.2, 'isiHalfRange':0,2,
+              'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1.2, 'isiHalfRange':0.2,
               'minInt':60, 'maxInt':60 }
 
 tuningFRA = {'subject':'pals023','experimenter':'jenny', 'minFreq':2000, 'maxFreq':40000,
@@ -8,4 +8,4 @@ tuningFRA = {'subject':'pals023','experimenter':'jenny', 'minFreq':2000, 'maxFre
 
 tuningAM = {'subject':'pals023','experimenter':'jenny', 'minFreq':4, 'maxFreq':128, 'numTones':11,
             'stimType':'AM', 'stimDur':0.5, 'isiMean':1.2, 'isiHalfRange':0.2, 'minInt':60,
-            'maxInt':60}}
+            'maxInt':60}
