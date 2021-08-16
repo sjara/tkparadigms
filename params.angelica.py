4dcd2c0e715a3707e719648c5904523ef7a1fe3c
Santiago Jaramillo <sjara@uoregon.edu>
2021-08-16 15:11:36 -0700

    Added params file for Angelica.

---
+++ params.angelica.py
@@ -0,0 +1,36 @@
+"""
+Params for Angelica.
+"""
+
+AM_stage_one = {'taskMode': 'water_on_sound',
+                'rewardSideMode': 'random',
+                'soundType':'AM_depth',
+                'experimenter': 'isabella'}
+
+AM_stage_two = {'taskMode': 'lick_on_stim',
+                'lickBeforeStimOffset': 'ignore',
+                'rewardSideMode': 'repeat_mistake',
+                'soundType': 'AM_depth',
+                'experimenter': 'isabella'}
+
+AM_stage_three = {'taskMode': 'discriminate_stim',
+                  'lickBeforeStimOffset': 'ignore',
+                  'rewardSideMode': 'repeat_mistake',
+                  'soundType': 'AM_depth',
+                  'experimenter': 'isabella'}
+
+AM_stage_four = {'taskMode': 'discriminate_stim',
+                 'rewardSideMode': 'random',
+                 'soundType': 'AM_depth',
+                 'experimenter': 'isabella'}
+
+
+pals015 = {'subject':'pals015',
+           'experimenter': 'angelica',
+           'taskMode': 'water_on_sound',
+           'rewardSideMode': 'random',
+           'soundType':'chords',
+           }
+
+
+
