6696a8f57b1aa5b99a5c6eabacf77e9b3b475f25
Angelica Mejia <87153505+amejia7@users.noreply.github.com>
2021-11-10 10:06:55 -0800

    Update params.angelica.py

--- params.angelica.py
+++ params.angelica.py
@@ -2,27 +2,38 @@
 Params for Angelica.
 """
 
-AM_stage_one = {'taskMode': 'water_on_sound',
-                'rewardSideMode': 'random',
-                'soundType':'AM_depth',
-                'experimenter': 'isabella'}
+AM_stage_zero = {'outcomeMode':'sides_direct',
+                'delayToTargetMean':0,
+                'delayToTargetHalfRange':0
+                'experimenter': 'angelica'
+               }
 
-AM_stage_two = {'taskMode': 'lick_on_stim',
-                'lickBeforeStimOffset': 'ignore',
-                'rewardSideMode': 'repeat_mistake',
-                'soundType': 'AM_depth',
-                'experimenter': 'isabella'}
+AM_stage_one = {'outcomeMode':'direct',
+                'delayToTargetMean':0,
+                'delayToTargetHalfRange':0
+                'experimenter': 'angelica'}
 
-AM_stage_three = {'taskMode': 'discriminate_stim',
-                  'lickBeforeStimOffset': 'ignore',
-                  'rewardSideMode': 'repeat_mistake',
-                  'soundType': 'AM_depth',
-                  'experimenter': 'isabella'}
+AM_stage_two = {'outcomeMode':'on_next_correct',
+                'delayToTargetMean':0,
+                'delayToTargetHalfRange':0
+                'automationMode': 'increase_delay'
+                'experimenter': 'angelica'}
 
-AM_stage_four = {'taskMode': 'discriminate_stim',
-                 'rewardSideMode': 'random',
-                 'soundType': 'AM_depth',
-                 'experimenter': 'isabella'}
+AM_stage_three = {'outcomeMode':'only_if_correct',
+                'delayToTargetMean':2,
+                'delayToTargetHalfRange':0.05
+                'experimenter': 'angelica'}
+
+bias_correction_stage = {'outcomeMode':'only_if_correct',
+                'delayToTargetMean':2,
+                'delayToTargetHalfRange':0.05
+                'experimenter': 'angelica'}
+
+AM_stage_four = {'outcomeMode':'only_if_correct',
+                'delayToTargetMean':2,
+                'delayToTargetHalfRange':0.05
+                 'psycurveMode': 'uniform'
+                'experimenter': 'angelica'}
 
 
 pals015 = {'subject':'pals015',
@@ -33,8 +44,28 @@ pals015 = {'subject':'pals015',
            }
 
 amod015 = {'subject':'amod015',
-           'experimenter': 'angelica',
           }
-amod015.update(AM_stage_one)
+
+amod016 = {'subject':'amod015',
+          }
+
+amod017 = {'subject':'amod015',
+          }
+
+amod018 = {'subject':'amod015',
+          }
+
+amod019 = {'subject':'amod015',
+          }
+
+amod019 = {'subject':'amod015',
+          }
+
+amod015.update(AM_stage_three)
+amod016.update(AM_stage_three)
+amod017.update(AM_stage_three)
+amod018.update(AM_stage_three)
+amod019.update(AM_stage_three)
+amod020.update(AM_stage_three)
 
 # python am_discrimination.py params.angelica.py amod015
