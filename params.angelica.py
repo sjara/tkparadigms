3f71d7f2c8f30676e11a062704f396ec68bf9e0b
Angelica Mejia <87153505+amejia7@users.noreply.github.com>
2021-12-04 14:18:57 -0800

    Updated daily training paradigms 04-12-2021

--- params.angelica.py
+++ params.angelica.py
@@ -63,8 +63,8 @@ amod020 = {'subject':'amod020',
           }
 
 amod015.update(AM_stage_four)
-amod016.update(bias_correction_stage)
-amod017.update(AM_stage_three)
+amod016.update(AM_stage_three)
+amod017.update(AM_stage_four)
 amod018.update(AM_stage_three)
 amod019.update(AM_stage_four)
 amod020.update(AM_stage_four)
