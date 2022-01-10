b08122f15ed55fb7ac72f5f9f5799f38a94ad1b3
Jenny Mohn <jlmohn@ucdavis.edu>
2022-01-10 12:07:06 -0800

    updated params.jenny.py

--- params.jenny.py
+++ params.jenny.py
@@ -1,11 +1,16 @@
-tuningFreq = {'subject':'feat001','experimenter':'jenny', 'minFreq':2000, 'maxFreq':40000,
-              'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1.2, 'isiHalfRange':0.2,
-              'minInt':60, 'maxInt':70, 'numInt':2 }
-
 tuningFRA = {'subject':'pals027','experimenter':'jenny', 'minFreq':2000, 'maxFreq':40000,
               'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1, 'isiHalfRange':0,
               'minInt':10, 'maxInt':60, 'numInt':6 }
 
-tuningAM = {'subject':'feat001','experimenter':'jenny', 'minFreq':4, 'maxFreq':128, 'numTones':11,
+tuningFreq = {'subject':'feat003','experimenter':'jenny', 'minFreq':2000, 'maxFreq':40000,
+              'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':60, 'maxInt':70, 'numInt':2 }
+
+
+tuningAM = {'subject':'feat003','experimenter':'jenny', 'minFreq':4, 'maxFreq':128, 'numTones':11,
             'stimType':'AM', 'stimDur':0.5, 'isiMean':1.2, 'isiHalfRange':0.2, 'minInt':60,
             'maxInt':60}
+            
+speechPilot = {'subject':'feat003', 'experimenter':'jenny','outcomeMode':'passive_exposure','delayToTargetMean':3.0,'delayToTargetHalfRange':1,'psycurveMode':'uniform'}
+
+speechTest = {'subject':'feat003', 'experimenter':'jenny','outcomeMode':'passive_exposure','delayToTargetMean':1.0,'delayToTargetHalfRange':0,'psycurveMode':'uniform'}
