287eeff7b16b8758ade4090c5cbf8fdd3db245cb
Jenny Mohn <jmohn@uoregon.edu>
2022-01-10 14:47:21 -0800

    added behavior params

--- params.jenny.py
+++ params.jenny.py
@@ -2,15 +2,81 @@ tuningFRA = {'subject':'pals027','experimenter':'jenny', 'minFreq':2000, 'maxFre
               'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1, 'isiHalfRange':0,
               'minInt':10, 'maxInt':60, 'numInt':6 }
 
-tuningFreq = {'subject':'feat003','experimenter':'jenny', 'minFreq':2000, 'maxFreq':40000,
+tuningFreq = {'subject':'feat004','experimenter':'jenny', 'minFreq':2000, 'maxFreq':40000,
               'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1.2, 'isiHalfRange':0.2,
-              'minInt':60, 'maxInt':70, 'numInt':2 }
+              'minInt':60, 'maxInt':60, 'numInt':2 }
 
 
-tuningAM = {'subject':'feat003','experimenter':'jenny', 'minFreq':4, 'maxFreq':128, 'numTones':11,
+tuningAM = {'subject':'feat004','experimenter':'jenny', 'minFreq':4, 'maxFreq':128, 'numTones':11,
             'stimType':'AM', 'stimDur':0.5, 'isiMean':1.2, 'isiHalfRange':0.2, 'minInt':60,
             'maxInt':60}
-            
-speechPilot = {'subject':'feat003', 'experimenter':'jenny','outcomeMode':'passive_exposure','delayToTargetMean':3.0,'delayToTargetHalfRange':1,'psycurveMode':'uniform'}
+
+speechPilot = {'subject':'feat004', 'experimenter':'jenny','outcomeMode':'passive_exposure','delayToTargetMean':3.0,'delayToTargetHalfRange':1,'psycurveMode':'uniform'}
 
 speechTest = {'subject':'feat003', 'experimenter':'jenny','outcomeMode':'passive_exposure','delayToTargetMean':1.0,'delayToTargetHalfRange':0,'psycurveMode':'uniform'}
+
+### Behavior ###
+## VOT ##
+VOTstage0 = {'experimenter':'jenny', 'outcomeMode':'sides_direct', 'relevantFeature':'temporal', 'delayToTargetMean':0, 'delayToTargetHalfRange':0}
+
+VOTstage1 = {'experimenter':'jenny', 'outcomeMode':'direct', 'relevantFeature':'temporal', 'delayToTargetMean':0, 'delayToTargetHalfRange':0}
+
+VOTstage2 = {'experimenter':'jenny', 'outcomeMode':'on_next_correct', 'relevantFeature':'temporal', 'delayToTargetMean':0.01, 'delayToTargetHalfRange':0.0,'automationMode':'increase_delay'}
+
+VOTstage3 = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'temporal', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05}
+
+VOTstage4 = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'temporal', 'psycurveMode':'uniform', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05}
+
+VOTBiasCorr = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'temporal', 'antibiasMode':'repeat_mistake', 'delayToTargetMean': 0.2, 'delayToTargetHalfRange':0.05}
+
+
+bili034 = {'subject':'bili034', 'soundActionMode':'low_left', **VOTstage0}
+
+bili035 = {'subject':'bili035', 'soundActionMode':'low_left', **VOTstage0}
+
+bili036 = {'subject':'bili036', 'soundActionMode':'low_left', **VOTstage0}
+
+bili037 = {'subject':'bili037', 'soundActionMode':'low_left', **VOTstage0}
+
+bili038 = {'subject':'bili038', 'soundActionMode':'low_left', **VOTstage0}
+
+bili039 = {'subject':'bili039', 'soundActionMode':'high_left', **VOTstage0}
+
+bili040 = {'subject':'bili040', 'soundActionMode':'high_left', **VOTstage0}
+
+bili041 = {'subject':'bili041', 'soundActionMode':'high_left', **VOTstage0}
+
+bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTstage0}
+
+
+## FT ##
+FTstage0 = {'experimenter':'jenny', 'outcomeMode':'sides_direct', 'relevantFeature':'spectral', 'delayToTargetMean':0, 'delayToTargetHalfRange':0}
+
+FTstage1 = {'experimenter':'jenny', 'outcomeMode':'direct', 'relevantFeature':'spectral', 'delayToTargetMean':0, 'delayToTargetHalfRange':0}
+
+FTstage2 = {'experimenter':'jenny', 'outcomeMode':'on_next_correct', 'relevantFeature':'spectral', 'delayToTargetMean':0.01, 'delayToTargetHalfRange':0.0,'automationMode':'increase_delay'}
+
+FTstage3 = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'spectral', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05}
+
+FTstage4 = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'spectral', 'psycurveMode':'uniform', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05}
+
+FTBiasCorr = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'spectral', 'antibiasMode':'repeat_mistake', 'delayToTargetMean': 0.2, 'delayToTargetHalfRange':0.05}
+
+
+bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTstage0}
+
+bili044 = {'subject':'bili044', 'soundActionMode':'low_left', **FTstage0}
+
+bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTstage0}
+
+bili046 = {'subject':'bili046', 'soundActionMode':'low_left', **FTstage0}
+
+bili047 = {'subject':'bili047', 'soundActionMode':'low_left', **FTstage0}
+
+bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTstage0}
+
+bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage0}
+
+bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTstage0}
+
+bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage0}
