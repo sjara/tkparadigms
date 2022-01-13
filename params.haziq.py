1d6ca952ae5b8bfc48e77c1d7d1440784db3a422
Muhammad Haziq <mjhaziq92@gmail.com>
2022-01-13 11:30:45 -0800

    update the parameter of test002

--- params.haziq.py
+++ params.haziq.py
@@ -4,3 +4,9 @@ test000 = {'subject':'test000','experimenter':'haziq',
            
 test001 = {'subject':'test000','experimenter':'haziq', 
            'outcomeMode':'direct', 'delayToTargetMean':0.2, 'psycurveMode':'uniform'}
+           
+test002 = {'subject':'test000','experimenter':'haziq', 
+           'outcomeMode':'on_next_correct', 'delayToTargetMean': 0.01,'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay'}
+           
+stage02 = {'subject':'test000','experimenter':'haziq', 
+           'outcomeMode':'on_next_correct', 'delayToTargetMean': 0.01,'delayToTargetHalfRange':0.0, 'psycurveMode':'uniform'}                               
