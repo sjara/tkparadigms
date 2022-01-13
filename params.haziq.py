21a20c074d0124b4883a9208ea19049dfbcd3370
Muhammad Haziq <mjhaziq92@gmail.com>
2022-01-13 13:21:33 -0800

    update parameter for stage 2,3 and 4

--- params.haziq.py
+++ params.haziq.py
@@ -1,12 +1,15 @@
 
-#pamo000 = {'subject':'test000','experimenter':'haziq', 
+#pamo000 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'sides_direct', 'delayToTargetMean':0.2}
            
-#pamo001 = {'subject':'test000','experimenter':'haziq', 
+#pamo001 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'direct', 'delayToTargetMean':0.2,}
            
-pamo002 = {'subject':'test000','experimenter':'haziq', 
-           'outcomeMode':'on_next_correct', 'delayToTargetMean': 0.01,'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay','maxNtrials':500}
+pamo002 = {'subject':'test000', 'experimenter':'haziq', 
+           'outcomeMode':'on_next_correct', 'delayToTargetMean': 0.01, 'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay', 'maxNtrials':500}
            
-pamo003 = {'subject':'test000','experimenter':'haziq', 
-           'outcomeMode':'only_if_correct', 'delayToTargetMean': 0.2,'delayToTargetHalfRange':0.05, 'psycurveMode':'off'}                               
+pamo003 = {'subject':'test000', 'experimenter':'haziq', 
+           'outcomeMode':'only_if_correct', 'delayToTargetMean': 0.2, 'delayToTargetHalfRange':0.05, 'psycurveMode':'off', 'maxNtrials':500}
+           
+pamo003 = {'subject':'test000', 'experimenter':'haziq', 
+           'outcomeMode':'only_if_correct', 'delayToTargetMean': 0.2, 'delayToTargetHalfRange':0.05, 'psycurveMode':'uniform', 'maxNtrials':500}                                  
