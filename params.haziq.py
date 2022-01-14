21ad64416bb761362de0858a36b028344c9f1cd7
Muhammad Haziq <mjhaziq92@gmail.com>
2022-01-13 17:51:38 -0800

    update parameter for stage 2

--- params.haziq.py
+++ params.haziq.py
@@ -5,8 +5,7 @@
 #pamo001 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'direct', 'delayToTargetMean':0.2,}
            
-test002 = {'subject':'test000', 'experimenter':'haziq', 
-           'outcomeMode':'on_next_correct', 'delayToTargetMean': 0.01, 'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay', 'maxNtrials':500}
+test002 = {'subject':'test000', 'experimenter':'haziq', 'outcomeMode':'on_next_correct', 'delayToTargetMean': 0.01, 'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay', 'maxNtrials':500}
            
 pamo003 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'only_if_correct', 'delayToTargetMean': 0.2, 'delayToTargetHalfRange':0.05, 'psycurveMode':'off', 'maxNtrials':500}
