dd4d49e02c6df0f45add8eac128e289e4cfd5330
Muhammad Haziq <mjhaziq92@gmail.com>
2022-01-13 18:05:07 -0800

    update parameter for stage 0,1 and 2

--- params.haziq.py
+++ params.haziq.py
@@ -3,7 +3,7 @@ pamo000 = {'subject':'test000', 'experimenter':'haziq',
            'outcomeMode':'sides_direct', 'delayToTargetMean':0.2}
            
 pamo001 = {'subject':'test000', 'experimenter':'haziq', 
-           'outcomeMode':'direct', 'delayToTargetMean':0.2,}
+           'outcomeMode':'direct', 'delayToTargetMean':0.2}
            
 pamo002 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'on_next_correct', 'delayToTargetMean':0.01,}
@@ -14,7 +14,7 @@ pamo003 = {'subject':'test000', 'experimenter':'haziq',
 
 pamo004 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'only_if_correct', 'delayToTargetMean':0.01,
-           'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay'
+           'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay',
            'maxNtrials':500}
                       
 test002 = {'subject':'test000', 'experimenter':'haziq', 'outcomeMode':'on_next_correct', 'delayToTargetMean': 0.01, 'delayToTargetHalfRange':0.0,'automationMode':'increase_delay', 'maxNtrials':500}
