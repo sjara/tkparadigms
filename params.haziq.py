6de643b8478aab98d1b3ad83cff92fb5f51def4c
Muhammad Haziq <mjhaziq92@gmail.com>
2022-01-21 10:21:17 -0800

    update parameter for all training stages

--- params.haziq.py
+++ params.haziq.py
@@ -1,23 +1,74 @@
 
-pamo000 = {'subject':'test000', 'experimenter':'haziq', 
-           'outcomeMode':'sides_direct', 'delayToTargetMean':0.2}
+stage0 = {'subject':'test000', 'experimenter':'haziq', 
+           'outcomeMode':'sides_direct', 'delayToTargetMean':0}
            
-pamo001 = {'subject':'test000', 'experimenter':'haziq', 
-           'outcomeMode':'direct', 'delayToTargetMean':0.2}
+stage1 = {'subject':'test000', 'experimenter':'haziq', 
+           'outcomeMode':'direct', 'delayToTargetMean':0}
            
-pamo002 = {'subject':'test000', 'experimenter':'haziq', 
+stage2 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'on_next_correct', 'delayToTargetMean':0.01,
-           'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay',
-           'maxNtrials':500}
+           'delayToTargetHalfRange':0.0, 'automationMode':'increase_delay'}
            
-pamo003 = {'subject':'test000', 'experimenter':'haziq', 
+stage3 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2,
            'delayToTargetHalfRange':0.05, 'psycurveMode':'off',
            'maxNtrials':500}
 
-pamo004 = {'subject':'test000', 'experimenter':'haziq', 
+stage4 = {'subject':'test000', 'experimenter':'haziq', 
            'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2,
            'delayToTargetHalfRange':0.05, 'psycurveMode':'uniform',
            'maxNtrials':500}
                       
+pamo009 = stage2
+pamo009['subject'] = 'pamo009'
 
+pamo010 = stage2
+pamo010['subject'] = 'pamo010'
+
+pamo011 = stage2
+pamo011['subject'] = 'pamo011'
+
+pamo012 = stage2
+pamo012['subject'] = 'pamo012'
+
+pamo013 = stage2
+pamo013['subject'] = 'pamo013'
+
+pamo014 = stage2
+pamo014['subject'] = 'pamo014'
+
+pamo015 = stage2
+pamo015['subject'] = 'pamo015'
+
+pamo016 = stage2
+pamo016['subject'] = 'pamo016'
+
+pamo017 = stage2
+pamo017['subject'] = 'pamo017'
+
+pamo018 = stage2
+pamo018['subject'] = 'pamo018'
+
+pamo019 = stage2
+pamo019['subject'] = 'pamo019'
+
+pamo020 = stage2
+pamo020['subject'] = 'pamo020'
+
+pamo021 = stage2
+pamo021['subject'] = 'pamo021'
+
+pamo022 = stage2
+pamo022['subject'] = 'pamo022'
+
+pamo023 = stage2
+pamo023['subject'] = 'pamo023'
+
+pamo024 = stage2
+pamo024['subject'] = 'pamo024'
+
+pamo025 = stage1
+pamo025['subject'] = 'pamo025'
+
+pamo026 = stage2
+pamo026['subject'] = 'pamo026'
