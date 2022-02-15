d94dd18c9079850fcbb1f1ade599b54c8bbe7c12
JaraLab User <jlmohn@ucdavis.edu>
2022-02-14 17:38:59 -0800

    updated bili params

--- params.jenny.py
+++ params.jenny.py
@@ -64,7 +64,7 @@ bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTstage3}
 
 bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage3}
 
-bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTBiasCorr}
+bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTstage3}
 
 bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage3}
 
@@ -76,13 +76,13 @@ bili040 = {'subject':'bili040', 'soundActionMode':'high_left', **VOTstage3}
 
 bili041 = {'subject':'bili041', 'soundActionMode':'high_left', **VOTstage4}
 
-bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTstage3}
+bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTBiasCorr}
 
 bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTstage3}
 
 bili044 = {'subject':'bili044', 'soundActionMode':'low_left', **FTstage3}
 
-bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTBiasCorr}
+bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTstage3}
 
 bili046 = {'subject':'bili046', 'soundActionMode':'low_left', **FTstage3}
 
