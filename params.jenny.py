852b62f48f9084a9ae40d45c59d5da4257eb3c64
Jenny Mohn <jlmohn@ucdavis.edu>
2022-03-06 08:51:24 -0800

    updated bili params

--- params.jenny.py
+++ params.jenny.py
@@ -69,11 +69,11 @@ bili038 = {'subject':'bili038', 'soundActionMode':'low_left', **VOTstage4}
 
 bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTstage3}
 
-bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage3}
+bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage3, **leftBias}
 
 bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTstage3}
 
-bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage3}
+bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage4}
 
 #PM
 
@@ -85,15 +85,15 @@ bili041 = {'subject':'bili041', 'soundActionMode':'high_left', **VOTstage4}
 
 bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTstage4}
 
-bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTstage3}
+bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTBiasCorr, **leftBias}
 
 bili044 = {'subject':'bili044', 'soundActionMode':'low_left', **FTstage3}
 
-bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTBiasCorr, **leftBias}
+bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTstage3}
 
-bili046 = {'subject':'bili046', 'soundActionMode':'low_left', **FTstage3}
+bili046 = {'subject':'bili046', 'soundActionMode':'low_left', **FTBiasCorr, **leftBias}
 
-bili047 = {'subject':'bili047', 'soundActionMode':'low_left', **FTstage3}
+bili047 = {'subject':'bili047', 'soundActionMode':'low_left', **FTstage3, **leftBias}
 
 ## Headfixed
 
