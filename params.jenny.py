ce54c8f0e9902b2702ac84ea1793167cc7662371
Jenny Mohn <jlmohn@ucdavis.edu>
2022-03-08 09:01:41 -0800

    updated bili params

--- params.jenny.py
+++ params.jenny.py
@@ -57,7 +57,7 @@ leftBias = {'timeWaterValveL':0.025}
 rightBias = {'timeWaterValveR':0.025}
 #AM
 
-bili034 = {'subject':'bili034', 'soundActionMode':'low_left', **VOTstage4}
+bili034 = {'subject':'bili034', 'soundActionMode':'low_left', **VOTstage5}
 
 bili035 = {'subject':'bili035', 'soundActionMode':'low_left', **VOTstage4}
 
@@ -67,13 +67,13 @@ bili037 = {'subject':'bili037', 'soundActionMode':'low_left', **VOTstage4}
 
 bili038 = {'subject':'bili038', 'soundActionMode':'low_left', **VOTstage3}
 
-bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTBiasCorr}
+bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTstage3}
 
-bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTBiasCorr, **leftBias}
+bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage3}
 
-bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTBiasCorr, **leftBias}
+bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTstage3}
 
-bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage3}
+bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage3, **leftBias}
 
 #PM
 
@@ -83,13 +83,13 @@ bili040 = {'subject':'bili040', 'soundActionMode':'high_left', **VOTstage4}
 
 bili041 = {'subject':'bili041', 'soundActionMode':'high_left', **VOTstage5}
 
-bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTstage4}
+bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTstage5}
 
-bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTstage3}
+bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTBiasCorr, **rightBias}
 
-bili044 = {'subject':'bili044', 'soundActionMode':'low_left', **FTstage3, **leftBias}
+bili044 = {'subject':'bili044', 'soundActionMode':'low_left', **FTstage4}
 
-bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTstage3}
+bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTstage3, **leftBias}
 
 bili046 = {'subject':'bili046', 'soundActionMode':'low_left', **FTstage3}
 
