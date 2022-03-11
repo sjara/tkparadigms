b988eadb4e05f48ade49193004f7fb29bdb4de3d
Jenny Mohn <jlmohn@ucdavis.edu>
2022-03-11 07:01:23 -0800

    updated bili params

--- params.jenny.py
+++ params.jenny.py
@@ -59,7 +59,7 @@ rightBias = {'timeWaterValveR':0.025}
 
 bili034 = {'subject':'bili034', 'soundActionMode':'low_left', **VOTstage5}
 
-bili035 = {'subject':'bili035', 'soundActionMode':'low_left', **VOTstage3}
+bili035 = {'subject':'bili035', 'soundActionMode':'low_left', **VOTstage4}
 
 bili036 = {'subject':'bili036', 'soundActionMode':'low_left', **VOTstage5}
 
@@ -67,13 +67,13 @@ bili037 = {'subject':'bili037', 'soundActionMode':'low_left', **VOTstage4}
 
 bili038 = {'subject':'bili038', 'soundActionMode':'low_left', **VOTstage4}
 
-bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTstage3, **leftBias}
+bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTstage3}
 
-bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage3, **leftBias}
+bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage3}
 
-bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTstage3}
+bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTstage3, **rightBias}
 
-bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage3, **leftBias}
+bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage3}
 
 #PM
 
@@ -83,13 +83,13 @@ bili040 = {'subject':'bili040', 'soundActionMode':'high_left', **VOTstage4}
 
 bili041 = {'subject':'bili041', 'soundActionMode':'high_left', **VOTstage5}
 
-bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTstage5}
+bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTstage4}
 
 bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTstage3}
 
 bili044 = {'subject':'bili044', 'soundActionMode':'low_left', **FTstage4}
 
-bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTstage3}
+bili045 = {'subject':'bili045', 'soundActionMode':'low_left', **FTstage3, **rightBias}
 
 bili046 = {'subject':'bili046', 'soundActionMode':'low_left', **FTstage3, **rightBias}
 
