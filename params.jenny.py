a654f84b96db89fcfe468e694369f1ff88d5446b
JaraLab User <jlmohn@ucdavis.edu>
2022-03-29 09:39:42 -0700

    updated bili params

--- params.jenny.py
+++ params.jenny.py
@@ -71,9 +71,9 @@ bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTstage3}
 
 bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage3}
 
-bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTstage3}
+bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTBiasCorr}
 
-bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage3}
+bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage4}
 
 #PM
 
@@ -85,7 +85,7 @@ bili041 = {'subject':'bili041', 'soundActionMode':'high_left', **VOTstage5}
 
 bili042 = {'subject':'bili042', 'soundActionMode':'high_left', **VOTstage5}
 
-bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTstage3}
+bili043 = {'subject':'bili043', 'soundActionMode':'low_left', **FTstage4}
 
 bili044 = {'subject':'bili044', 'soundActionMode':'low_left', **FTstage5}
 
