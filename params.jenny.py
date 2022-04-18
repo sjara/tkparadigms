0012f98d7869bcab30899ba80dcf2e214d175dd6
Jenny Mohn <jlmohn@ucdavis.edu>
2022-04-18 10:51:44 -0700

    updated bili params

--- params.jenny.py
+++ params.jenny.py
@@ -73,7 +73,7 @@ bili048 = {'subject':'bili048', 'soundActionMode':'high_left', **FTstage3}
 
 bili049 = {'subject':'bili049', 'soundActionMode':'high_left', **FTstage4}
 
-bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTBiasCorr}
+bili050 = {'subject':'bili050', 'soundActionMode':'high_left', **FTstage3}
 
 bili051 = {'subject':'bili051', 'soundActionMode':'high_left', **FTstage5}
 
