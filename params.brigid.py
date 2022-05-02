a3387229551c413a76f18ebf369ce492803e1ce3
JaraLab User <jlmohn@ucdavis.edu>
2022-05-02 15:00:00 -0700

    created stages for psychometrics

--- params.brigid.py
+++ params.brigid.py
@@ -10,9 +10,13 @@ febeStage3 = {'experimenter':'jenny', 'relevantFeature':'temporal',  'psycurveMo
 
 febeStage4 = {'experimenter':'jenny', 'relevantFeature':'temporal',  'psycurveMode':'off', 'taskMode':'discriminate_stim', 'rewardSideMode':'random', 'lickBeforeStimOffset':'ignore', 'lickingPeriod':1.5, 'rewardAvailability':1, 'delayToTargetMean':2.5,'delayToTargetHalfRange':1,'targetMaxIntensity':60, 'timeWaterValve':0.03, 'stimType':'sound_only'}
 
+febeStage5 = {'experimenter':'jenny', 'relevantFeature':'temporal',  'psycurveMode':'extreme80pc', 'psycurveNsteps':6, 'taskMode':'discriminate_stim', 'rewardSideMode':'random', 'lickBeforeStimOffset':'ignore', 'lickingPeriod':1.5, 'rewardAvailability':1, 'delayToTargetMean':2.5,'delayToTargetHalfRange':1,'targetMaxIntensity':60, 'timeWaterValve':0.03, 'stimType':'sound_only'}
+
+febeStage6 = {'experimenter':'jenny', 'relevantFeature':'temporal',  'psycurveMode':'uniform', 'psycurveNsteps':6, 'taskMode':'discriminate_stim', 'rewardSideMode':'random', 'lickBeforeStimOffset':'ignore', 'lickingPeriod':1.5, 'rewardAvailability':1, 'delayToTargetMean':2.5,'delayToTargetHalfRange':1,'targetMaxIntensity':60, 'timeWaterValve':0.03, 'stimType':'sound_only'}
+
+
 febe001 = {'subject':'febe001', **febeStage1}
 febe007 = {'subject':'febe007', **febeStage2}
 febe008 = {'subject':'febe008', **febeStage4}
 febe009 = {'subject':'febe009', **febeStage4}
 febe012 = {'subject':'febe012', **febeStage2}
-
