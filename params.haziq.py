f5462d014ae577fb1d9aafa4bbb63f2a394e9089
MJHaziq <97411988+MJHaziq@users.noreply.github.com>
2022-05-20 20:55:22 -0700

    Update params.haziq.py
    update parameters for cohort 3 S3 active training

--- params.haziq.py
+++ params.haziq.py
@@ -116,16 +116,16 @@ pamo024.update(stage5)
 pamo027.update(stage3);
 pamo028.update(stage3)
 pamo029.update(stage3)
-pamo030.update(stage3)# performed more than 30% with antibias mode on (Thirteen day) 
-pamo031.update(stage3); pamo031.update(antibias) # Thirteen day 
+pamo030.update(stage3) 
+pamo031.update(stage3)# performed more than 30% with antibias mode on (Fourteen day) 
 pamo032.update(stage3) 
-pamo033.update(stage3); pamo033.update(antibias) # Thirteen day 
+pamo033.update(stage3)# performed more than 30% with antibias mode on (Fourteen day) 
 pamo034.update(stage3)  
-pamo035.update(stage3)# performed more than 30% with antibias mode on (Thirteen day)
+pamo035.update(stage3)
 pamo036.update(stage3); 
 pamo037.update(stage3)
 pamo038.update(stage3) 
 pamo039.update(stage3) 
 pamo040.update(stage3) 
 pamo041.update(stage3) 
-
+#; pamo031.update(antibias) # Thirteen day
