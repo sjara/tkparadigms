e661e33bed689e7af1f681156a2bfbbfb8199125
JaraLab User <jlmohn@ucdavis.edu>
2022-06-07 13:45:09 -0700

    updated febe params

--- params.brigid.py
+++ params.brigid.py
@@ -18,7 +18,7 @@ febeStage7 = {'experimenter':'jenny', 'relevantFeature':'temporal', 'irrelevantF
 
 
 febe001 = {'subject':'febe001', **febeStage1}
-febe007 = {'subject':'febe007', **febeStage7}
+febe007 = {'subject':'febe007', **febeStage6}
 febe008 = {'subject':'febe008', **febeStage7}
 febe009 = {'subject':'febe009', **febeStage7}
 febe012 = {'subject':'febe012', **febeStage7}
