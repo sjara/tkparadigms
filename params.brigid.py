75168172e1da313b2a5854496aa6271448a0ffc0
brigiddeck <102103797+brigiddeck@users.noreply.github.com>
2022-06-22 20:21:36 -0700

    Update params.brigid.py

--- params.brigid.py
+++ params.brigid.py
@@ -20,5 +20,5 @@ febeStage7 = {'experimenter':'jenny', 'relevantFeature':'temporal', 'irrelevantF
 febe001 = {'subject':'febe001', **febeStage1}
 febe007 = {'subject':'febe007', **febeStage5}
 febe008 = {'subject':'febe008', **febeStage7}
-febe009 = {'subject':'febe009', **febeStage3}
+febe009 = {'subject':'febe009', **febeStage2}
 febe012 = {'subject':'febe012', **febeStage7}
