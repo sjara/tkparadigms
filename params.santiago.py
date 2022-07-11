6ebbfcbce9df8807770a744bfc53d35747065f00
Santiago Jaramillo <sjara@uoregon.edu>
2022-07-10 22:35:01 -0700

    Added pure014 and pure015.

--- params.santiago.py
+++ params.santiago.py
@@ -81,3 +81,13 @@ fm000 = {'subject':'test000', 'taskMode':'discriminate_stim', 'interTrialInterva
          'soundType':'FM_direction', 'highFreq':1300, 'lowFreq':600, 'rewardSideMode':'toggle'}
 
 speech000 = {'subject':'test000', 'outcomeMode':'passive_exposure'}
+
+
+quicktrials = {'interTrialIntervalMean':2, 'interTrialIntervalHalfRange':0}
+
+detect_sound_stage1 = {'experimenter':'santiago', 'taskMode':'water_after_sound'}
+detect_sound_stage2 = {'experimenter':'santiago', 'taskMode':'detect_single_sound'}
+
+pure000 = {'subject': 'pure000', **detect_sound_stage1, **quicktrials}
+pure014 = {'subject': 'pure014', **detect_sound_stage1}
+pure015 = {'subject': 'pure015', **detect_sound_stage1}
