b6a0dc6ce1dbff50444d3bdfd5259b9258c0650d
Santiago Jaramillo <sjara@uoregon.edu>
2022-07-19 23:16:45 -0700

    Changed pure mice to Stage 4 (two distractors).

--- params.santiago.py
+++ params.santiago.py
@@ -88,9 +88,13 @@ quicktrials = {'interTrialIntervalMean':1, 'interTrialIntervalHalfRange':0}
 detect_sound_stage1 = {'experimenter':'santiago', 'taskMode':'water_after_sound'}
 detect_sound_stage2 = {'experimenter':'santiago', 'taskMode':'detect_single_sound'}
 detect_sound_stage3 = {'experimenter':'santiago', 'taskMode':'detect_with_distractors',
-                       'punishmentType':'noise', 'punishmentIntensity':50,
+                       'distractorType':'lower', 'punishmentType':'noise', 'punishmentIntensity':50,
+                       'interTrialIntervalMean':2, 'interTrialIntervalHalfRange':0.5}
+detect_sound_stage4 = {'experimenter':'santiago', 'taskMode':'detect_with_distractors',
+                       'distractorType':'lower_and_higher', 'punishmentType':'noise',
+                       'punishmentIntensity':50,
                        'interTrialIntervalMean':2, 'interTrialIntervalHalfRange':0.5}
 
-pure000 = {'subject': 'pure000', **detect_sound_stage3, **quicktrials}
-pure014 = {'subject': 'pure014', **detect_sound_stage3}
-pure015 = {'subject': 'pure015', **detect_sound_stage3}
+pure000 = {'subject': 'pure000', **detect_sound_stage4, **quicktrials}
+pure014 = {'subject': 'pure014', **detect_sound_stage4}
+pure015 = {'subject': 'pure015', **detect_sound_stage4}
