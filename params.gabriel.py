b6b5cc81fcfbca2b564a66b48c3fc022d3c9c370
Gtoea <91916146+Gtoea@users.noreply.github.com>
2022-07-21 17:03:21 -0700

    Update params.gabriel.py

--- params.gabriel.py
+++ params.gabriel.py
@@ -18,7 +18,7 @@ febeSpectralStage7 = {'experimenter':'gabriel', 'relevantFeature':'spectral', 'i
 
 
 
-febe013 = {'subject':'febe013', **febeSpectralStage4}
-febe019 = {'subject':'febe019', **febeSpectralStage3}
-febe020 = {'subject':'febe020', **febeSpectralStage3}
-febe021 = {'subject':'febe021', **febeSpectralStage3}
+febe013 = {'subject':'febe013', **febeSpectralStage2}
+febe019 = {'subject':'febe019', **febeSpectralStage2}
+febe020 = {'subject':'febe020', **febeSpectralStage2}
+febe021 = {'subject':'febe021', **febeSpectralStage2}
