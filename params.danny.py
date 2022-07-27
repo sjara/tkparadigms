71d8e69389b970f3b4a152141e86a9748e491ad3
Danny Brown <dabrown@uoregon.edu>
2022-07-27 08:50:32 -0700

    Updated danny params file.

---
+++ params.danny.py
@@ -0,0 +1,20 @@
+dbtest1 = {'experimenter':'danny', 'interTrialIntervalMean':10,
+             'interTrialIntervalHalfRange':2, 'preDurationMean':7, 'preDurationHalfRange':0,
+             'totalStimDuration':12, 'fadeIn':2, 'maxFreq':15000, 'minFreq':4000,
+             'nFreqs':3, 'minFreqRatio':1.00, 'soundIntensity':70}
+             
+dbtest2 = {'experimenter':'danny', 'interTrialIntervalMean':10,
+             'interTrialIntervalHalfRange':2, 'preDurationMean':7, 'preDurationHalfRange':0,
+             'totalStimDuration':12, 'fadeIn':2, 'maxFreq':15000, 'minFreq':4000,
+             'nFreqs':6, 'minFreqRatio':1.00, 'soundIntensity':70}
+             
+dbtest3 = {'experimenter':'danny', 'interTrialIntervalMean':10,
+             'interTrialIntervalHalfRange':2, 'preDurationMean':7, 'preDurationHalfRange':0,
+             'totalStimDuration':12, 'fadeIn':2, 'maxFreq':15000, 'minFreq':4000,
+             'nFreqs':6, 'minFreqRatio':1.00, 'soundIntensity':70}
+             
+# Note: dbtest2 and dbtest3 are exactly the same; may want to consolidate them to avoid confusion.
+
+dbtest4 = {'experimenter':'danny', 'maxFreq':15000, 'minFreq':4000, 'numTones':6,
+             'minInt':70, 'maxInt':70, 'numInt':1, 'stimDur':5,'isiMean':10, 'isiHalfRange':1,
+             'randomMode':'Random', 'stimType':'Chord'} 
