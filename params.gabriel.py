b6b03c97ad5e6e4c45a055d3b0aa704baaf9551e
Gtoea <91916146+Gtoea@users.noreply.github.com>
2022-07-30 08:51:41 -0700

    Moved Febe019 to stage 3 and Febe021 to bias mode

--- params.gabriel.py
+++ params.gabriel.py
@@ -19,8 +19,8 @@ febeSpectralStage6 = {'experimenter':'gabriel', 'relevantFeature':'spectral', 'i
 
 
 febe013 = {'subject':'febe013', **febeSpectralStage3}
-febe019 = {'subject':'febe019', **febeSpectralStageBiasCorrect}
+febe019 = {'subject':'febe019', **febeSpectralStage3}
 febe020 = {'subject':'febe020', **febeSpectralStage4}
-febe021 = {'subject':'febe021', **febeSpectralStage3}
+febe021 = {'subject':'febe021', **febeSpectralStageBiasCorrect}
 febe007 = {'subject':'febe021', **febeSpectralStage3}
 febe012 = {'subject':'febe021', **febeSpectralStage3}
