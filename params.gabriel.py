bdcb2d1ced3888114805440923f842f5194fd821
Gtoea <91916146+Gtoea@users.noreply.github.com>
2022-08-03 13:42:28 -0700

    Moved Febe013 back onto Stage 3

--- params.gabriel.py
+++ params.gabriel.py
@@ -18,9 +18,9 @@ febeSpectralStage6 = {'experimenter':'gabriel', 'relevantFeature':'spectral', 'i
 
 
 
-febe013 = {'subject':'febe013', **febeSpectralStageBiasCorrect}
+febe013 = {'subject':'febe013', **febeSpectralStage3}
 febe019 = {'subject':'febe019', **febeSpectralStage3}
 febe020 = {'subject':'febe020', **febeSpectralStage5}
-febe021 = {'subject':'febe021', **febeSpectralStage3}
+febe021 = {'subject':'febe021', **febeSpectralStageBiasCorrect}
 febe007 = {'subject':'febe007', **febeSpectralStage3}
-febe012 = {'subject':'febe012', **febeSpectralStage3}
+febe012 = {'subject':'febe012', **febeSpectralStageBiasCorrect}
