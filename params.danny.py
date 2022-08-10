6f4da3fe3fedbc2531a4758dd973e2708414a7e9
JaraLab User <dab22008@gmail.com>
2022-08-10 16:42:34 -0700

    Added AM params

--- params.danny.py
+++ params.danny.py
@@ -1,22 +1,28 @@
-dbtest1 = {'experimenter':'danny', 'interTrialIntervalMean':10,
+lowestfirst3chord = {'experimenter':'danny', 'interTrialIntervalMean':10,
            'interTrialIntervalHalfRange':2, 'preDurationMean':7, 'preDurationHalfRange':0,
            'totalStimDuration':12, 'fadeIn':2, 'maxFreq':15000, 'minFreq':4000,
            'nFreqs':3, 'minFreqRatio':1.00, 'soundIntensity':70}
+  #^previously: dbtest1
              
-dbtest2 = {'experimenter':'danny', 'interTrialIntervalMean':10,
+lowestfirst6chord = {'experimenter':'danny', 'interTrialIntervalMean':10,
            'interTrialIntervalHalfRange':2, 'preDurationMean':7, 'preDurationHalfRange':0,
            'totalStimDuration':12, 'fadeIn':2, 'maxFreq':15000, 'minFreq':4000,
            'nFreqs':6, 'minFreqRatio':1.00, 'soundIntensity':70}
-             
-dbtest3 = {'experimenter':'danny', 'interTrialIntervalMean':10,
-           'interTrialIntervalHalfRange':2, 'preDurationMean':7, 'preDurationHalfRange':0,
-           'totalStimDuration':12, 'fadeIn':2, 'maxFreq':15000, 'minFreq':4000,
-           'nFreqs':6, 'minFreqRatio':1.00, 'soundIntensity':70}
-             
-# Note: dbtest2 and dbtest3 are exactly the same; may want to consolidate them to avoid confusion.
+  #^previously: dbtest2             
 
-dbtest4 = {'experimenter':'danny', 'maxFreq':15000, 'minFreq':4000, 'numTones':6,
+highestfirst6chord = {'experimenter':'danny', 'interTrialIntervalMean':10,
+           'interTrialIntervalHalfRange':2, 'preDurationMean':7, 'preDurationHalfRange':0,
+           'totalStimDuration':12, 'fadeIn':2, 'maxFreq':15000, 'minFreq':4000,
+           'nFreqs':6, 'minFreqRatio':1.00, 'soundIntensity':70}
+  #^previously: dbtest3            
+
+random6chord = {'experimenter':'danny', 'maxFreq':15000, 'minFreq':4000, 'numTones':6,
            'minInt':70, 'maxInt':70, 'numInt':1, 'stimDur':5,'isiMean':10, 'isiHalfRange':1,
            'randomMode':'Random', 'stimType':'Chord'} 
-           
-# adding this test for GitHub testing.
+  #^previously: dbtest4           
+
+# Amplitude Modulated Sounds
+
+AMrandom3rate = {'soundType':'AM_rate', 'soundSetMode':'random', 'minFreq':4, 'maxFreq':12,
+           'nFreqs':3,'experimenter':'danny', 'interTrialIntervalMean':10,  'preDurationMean':7,
+           'fadeIn':2, 'totalStimDuration':12, 'minFreqRatio':1.00, 'soundIntensity':70}
