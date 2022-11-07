c059a3dffad9a97121c6e11a1eea81431aa24d86
brigiddeck <102103797+brigiddeck@users.noreply.github.com>
2022-11-07 12:51:37 -0800

    Update params.brigid.py

--- params.brigid.py
+++ params.brigid.py
@@ -9,8 +9,8 @@ coopStage2 = {'taskMode':'reward_on_first_poke', 'waitTime':3}
 coopStage3 = {'taskMode':'reward_on_last_poke', 'waitTime':3}
 coopStage4 = {'taskMode':'cooperate', 'waitTime':3}
 
-coop006x007 = {'subject':'coop006x007', 'experimenter':'brigid', **coopStage3}
-coop008x009 = {'subject':'coop008x009', 'experimenter':'brigid', **coopStage3}
+coop006x007 = {'subject':'coop006x007', 'experimenter':'brigid', **coopStage4}
+coop008x009 = {'subject':'coop008x009', 'experimenter':'brigid', **coopStage4}
 
 
 # --- Headfixed ---
