f978b7cc71a89771b7cb08376267bf41571ce8a7
Santiago Jaramillo <sjara@uoregon.edu>
2022-11-07 23:59:50 -0800

    It now stores outcomes and time of pokes.

--- cooperate_four_ports.py
+++ cooperate_four_ports.py
@@ -1,6 +1,10 @@
 '''
 Cooperate_four_ports is a paradigm in which two animals must poke simultaneously to obtain reward.
 There are two lanes (one for each animal) with one port at each end.
+
+TO DO:
+- Store timing of pokes on the wrong side.
+
 '''
 
 import numpy as np
@@ -59,9 +63,9 @@ class Paradigm(QtWidgets.QMainWindow):
        
         self.params['waitTime'] = paramgui.NumericParam('Wait time',value=3,
                                                         units='s',group='Timing parameters')
-        self.params['timeLEDon'] = paramgui.NumericParam('Time LED on',value=3,
-                                                         units='s',group='Timing parameters',
-                                                         enabled=False)
+        #self.params['timeLEDon'] = paramgui.NumericParam('Time LED on',value=3,
+        #                                                 units='s',group='Timing parameters',
+        #                                                 enabled=False)
         self.params['ITI'] = paramgui.NumericParam('Inter-trial interval',value=0,
                                                    units='s',group='Timing parameters',
                                                    enabled=True)
@@ -116,8 +120,10 @@ class Paradigm(QtWidgets.QMainWindow):
         # -- Add variables for storing results --
         maxNtrials = MAX_N_TRIALS # Preallocating space for each vector makes things easier
         self.results = utils.EnumContainer()
-        self.results.labels['outcome'] = {'aborted':0, 'rewarded':1, 'poke1only':2, 'poke2only':3, 'none':-1}
-        self.results['outcome'] = np.empty(maxNtrials,dtype=int)
+        self.results.labels['outcome'] = {'aborted':0, 'rewardedBoth':1, 'poke1only':2, 'poke2only':3, 'none':4}
+        self.results['outcome'] = np.empty(maxNtrials, dtype=int)
+        self.results['timePoke1'] = np.empty(maxNtrials, dtype=float)
+        self.results['timePoke2'] = np.empty(maxNtrials, dtype=float)
         #self.results.labels['activeSide'] = {'north':0,'south':1}
         #self.results['activeSide'] = np.empty(maxNtrials,dtype=int)
         #self.results['activeSide'][0] = 0
@@ -160,13 +166,13 @@ class Paradigm(QtWidgets.QMainWindow):
             self.calculate_results(nextTrial-1)
             previousOutcome = self.results['outcome'][nextTrial-1]
         else:
-            previousOutcome = self.results.labels['outcome']['rewarded']
+            previousOutcome = self.results.labels['outcome']['rewardedBoth']
             
         # -- Prepare next trial --
         taskMode = self.params['taskMode'].get_string()
         nextPortAfterFail = self.params['nextPortAfterFail'].get_string()
         waitTime = self.params['waitTime'].get_value()
-        timeLEDon = self.params['timeLEDon'].get_value()
+        #timeLEDon = self.params['timeLEDon'].get_value()
         interTrialInterval = self.params['ITI'].get_value()
         timeWaterValves = self.params['timeWaterValves'].get_value()
         #timeWaterValvesN = self.params['timeWaterValvesN'].get_value()
@@ -221,7 +227,7 @@ class Paradigm(QtWidgets.QMainWindow):
                               transitions={'Tup':'readyForNextTrial'},
                               outputsOff=[Water1, Water2])
         elif taskMode == 'auto_lights':
-            self.params['timeLEDon'].set_value(waitTime)
+            #self.params['timeLEDon'].set_value(waitTime)
             self.sm.set_extratimer('lightTimer1', duration=waitTime)
             self.sm.add_state(name='startTrial', statetimer=0,
                               transitions={'Tup':'waitForAnyPoke'},
@@ -269,7 +275,7 @@ class Paradigm(QtWidgets.QMainWindow):
                               transitions={'Tup':'readyForNextTrial'},
                               outputsOff=[LED1, LED2, Water1, Water2])
         elif taskMode == 'reward_on_first_poke':
-            self.params['timeLEDon'].set_value(waitTime)
+            #self.params['timeLEDon'].set_value(waitTime)
             self.sm.set_extratimer('lightTimer1', duration=waitTime)
             self.sm.set_extratimer('lightTimer2', duration=waitTime)
             self.sm.add_state(name='startTrial', statetimer=0,
@@ -318,7 +324,7 @@ class Paradigm(QtWidgets.QMainWindow):
                               transitions={'Tup':'readyForNextTrial'},
                               outputsOff=[LED1, LED2, Water1, Water2])
         elif taskMode == 'reward_on_last_poke':
-            self.params['timeLEDon'].set_value(waitTime)
+            #self.params['timeLEDon'].set_value(waitTime)
             self.sm.set_extratimer('lightTimer1', duration=waitTime)
             self.sm.set_extratimer('lightTimer2', duration=waitTime)
             self.sm.add_state(name='startTrial', statetimer=0,
@@ -353,7 +359,7 @@ class Paradigm(QtWidgets.QMainWindow):
                               transitions={'Tup':'readyForNextTrial'},
                               outputsOff=[LED1, LED2, Water1, Water2])
         elif taskMode == 'cooperate':
-            self.params['timeLEDon'].set_value(self.params['timeWaterValves'].get_value())
+            #self.params['timeLEDon'].set_value(self.params['timeWaterValves'].get_value())
             self.sm.add_state(name='startTrial', statetimer=0,
                               transitions={'Tup':'waitForPoke'},
                               outputsOff=[LED1, LED2])
@@ -372,73 +378,83 @@ class Paradigm(QtWidgets.QMainWindow):
             self.sm.add_state(name='iti', statetimer=interTrialInterval,
                               transitions={'Tup':'readyForNextTrial'},
                               outputsOff=[LED1, LED2, Water1, Water2])
-        elif taskMode == 'OLDcooperate':
-            self.sm.add_state(name='startTrial', statetimer=0,
-                              transitions={'Tup':'waitForPoke'},
-                              outputsOff=[LED1,LED2])
-            self.sm.add_state(name='waitForPoke', statetimer=LONGTIME,
-                              transitions={port1in:'waitForPoke2', port2in:'waitForPoke1'})
-            self.sm.add_state(name='waitForPoke2', statetimer=waitTime,
-                              transitions={port2in:'reward', 'Tup':'singlePoke'},
-                              outputsOn=[])
-            self.sm.add_state(name='waitForPoke1', statetimer=waitTime,
-                              transitions={port1in:'reward', 'Tup':'singlePoke'},
-                              outputsOn=[])
-            self.sm.add_state(name='singlePoke', statetimer=0,
-                              transitions={'Tup':'readyForNextTrial'},
-                              outputsOff=[LED1,LED2])
-            self.sm.add_state(name='reward', statetimer=timeWaterValves,
-                              transitions={'Tup':'stopReward'},
-                              outputsOn=[Water1, Water2, LED1, LED2],
-                              outputsOff=[])
-            self.sm.add_state(name='stopReward', statetimer=0,
-                              transitions={'Tup':'keepLEDon'},
-                              outputsOff=[Water1, Water2])
-            self.sm.add_state(name='keepLEDon', statetimer=timeLEDon,
-                              transitions={'Tup':'turnLEDoff'},
-                              outputsOff=[])
-            self.sm.add_state(name='turnLEDoff', statetimer=0,
-                              transitions={'Tup':'readyForNextTrial'},
-                              outputsOff=[LED1, LED2])
         
         #print(self.sm)
         self.dispatcher.set_state_matrix(self.sm)
         self.dispatcher.ready_to_start_trial()
 
         
-    def calculate_results(self,trialIndex):
+    def calculate_results(self, trialIndex):
         eventsThisTrial = self.dispatcher.events_one_trial(trialIndex)
         statesThisTrial = eventsThisTrial[:,2]
         taskMode = self.params['taskMode'].get_string()
+        # -- Check if it's an aborted trial --
+        lastEvent = eventsThisTrial[-1,:]
+        if lastEvent[1]==-1 and lastEvent[2]==0:
+            self.results['outcome'][trialIndex] = self.results.labels['outcome']['aborted']
+            self.results['timePoke1'][trialIndex] = np.nan
+            self.results['timePoke2'][trialIndex] = np.nan
+            return
         if taskMode in ['auto_lights', 'reward_on_first_poke']:
-            if ((self.sm.statesNameToIndex['reward1'] in statesThisTrial) or
-                (self.sm.statesNameToIndex['reward1after2'] in statesThisTrial)):
+            if (self.sm.statesNameToIndex['reward1'] in statesThisTrial):
                 self.params['nRewarded1'].add(1)
-                self.results['outcome'][trialIndex] = self.results.labels['outcome']['rewarded']
-            if ((self.sm.statesNameToIndex['reward2'] in statesThisTrial) or
-                  (self.sm.statesNameToIndex['reward2after1'] in statesThisTrial)):
+                eventInd = np.flatnonzero(statesThisTrial==self.sm.statesNameToIndex['reward1'])[0]
+                self.results['timePoke1'][trialIndex] = eventsThisTrial[eventInd,0]
+                if (self.sm.statesNameToIndex['reward2after1'] in statesThisTrial):
+                    self.params['nRewarded2'].add(1)
+                    eventInd = np.flatnonzero(statesThisTrial==self.sm.statesNameToIndex['reward2after1'])[0]
+                    self.results['timePoke2'][trialIndex] = eventsThisTrial[eventInd,0]
+                    self.results['outcome'][trialIndex] = self.results.labels['outcome']['rewardedBoth']
+                else:
+                    self.results['outcome'][trialIndex] = self.results.labels['outcome']['poke1only']
+                    self.results['timePoke2'][trialIndex] = np.nan
+            elif (self.sm.statesNameToIndex['reward2'] in statesThisTrial):
                 self.params['nRewarded2'].add(1)
-                self.results['outcome'][trialIndex] = self.results.labels['outcome']['rewarded']
-        else:
-            if self.sm.statesNameToIndex['reward'] in statesThisTrial:
-                self.params['nRewarded1'].add(1)
-                self.params['nRewarded2'].add(1)
-                self.results['outcome'][trialIndex] = self.results.labels['outcome']['rewarded']
-        #if self.sm.statesNameToIndex['waitForPoke2'] in statesThisTrial:
-        #    self.results['outcome'][trialIndex] = self.results.labels['outcome']['poke1only']
-        #if self.sm.statesNameToIndex['waitForPoke1'] in statesThisTrial:
-        #    self.results['outcome'][trialIndex] = self.results.labels['outcome']['poke2only']
-        self.results['outcome'][trialIndex] = self.results.labels['outcome']['none']
-        #else:
-        #    # This should not happen
-        #    self.results['outcome'][trialIndex] = self.results.labels['outcome']['none']
-        '''
-        if self.sm.statesNameToIndex['reward'] in statesThisTrial:
-            self.params['nRewarded1'].add(1)
-            self.params['nRewarded2'].add(1)
-            self.results['outcome'][trialIndex] = self.results.labels['outcome']['rewarded']
-        '''
-        
+                eventInd = np.flatnonzero(statesThisTrial==self.sm.statesNameToIndex['reward2'])[0]
+                self.results['timePoke2'][trialIndex] = eventsThisTrial[eventInd,0]
+                if (self.sm.statesNameToIndex['reward1after2'] in statesThisTrial):
+                    self.params['nRewarded1'].add(1)
+                    self.results['timePoke1'][trialIndex] = eventsThisTrial[eventInd,0]
+                    self.results['outcome'][trialIndex] = self.results.labels['outcome']['rewardedBoth']
+                else:
+                    self.results['outcome'][trialIndex] = self.results.labels['outcome']['poke2only']
+                    self.results['timePoke1'][trialIndex] = np.nan
+            else:
+                self.results['outcome'][trialIndex] = self.results.labels['outcome']['none']
+                self.results['timePoke1'][trialIndex] = np.nan
+                self.results['timePoke2'][trialIndex] = np.nan
+        if taskMode in ['reward_on_last_poke', 'cooperate']:
+            if (self.sm.statesNameToIndex['waitForPoke2'] in statesThisTrial):
+                eventInd = np.flatnonzero(statesThisTrial==self.sm.statesNameToIndex['waitForPoke2'])[0]
+                self.results['timePoke1'][trialIndex] = eventsThisTrial[eventInd,0]
+                if (self.sm.statesNameToIndex['reward'] in statesThisTrial):
+                    self.results['outcome'][trialIndex] = self.results.labels['outcome']['rewardedBoth']
+                    self.params['nRewarded1'].add(1)
+                    self.params['nRewarded2'].add(1)
+                    eventInd = np.flatnonzero(statesThisTrial==self.sm.statesNameToIndex['reward'])[0]
+                    self.results['timePoke2'][trialIndex] = eventsThisTrial[eventInd,0]
+                else:
+                    self.results['outcome'][trialIndex] = self.results.labels['outcome']['poke1only']
+                    self.results['timePoke2'][trialIndex] = np.nan
+            elif (self.sm.statesNameToIndex['waitForPoke1'] in statesThisTrial):
+                eventInd = np.flatnonzero(statesThisTrial==self.sm.statesNameToIndex['waitForPoke1'])[0]
+                self.results['timePoke2'][trialIndex] = eventsThisTrial[eventInd,0]
+                if (self.sm.statesNameToIndex['reward'] in statesThisTrial):
+                    self.results['outcome'][trialIndex] = self.results.labels['outcome']['rewardedBoth']
+                    self.params['nRewarded1'].add(1)
+                    self.params['nRewarded2'].add(1)
+                    eventInd = np.flatnonzero(statesThisTrial==self.sm.statesNameToIndex['reward'])[0]
+                    self.results['timePoke1'][trialIndex] = eventsThisTrial[eventInd,0]
+                else:
+                    self.results['outcome'][trialIndex] = self.results.labels['outcome']['poke2only']
+                    self.results['timePoke1'][trialIndex] = np.nan
+            else:
+                # -- This should never happen, since a trial requires a poke --
+                self.results['outcome'][trialIndex] = self.results.labels['outcome']['none']
+                self.results['timePoke1'][trialIndex] = np.nan
+                self.results['timePoke2'][trialIndex] = np.nan
+
+
     def closeEvent(self, event):
         '''
         Executed when closing the main window.
