4c6cd17637dedd145aef82ef0d28ec97bc817c41
Jenny Mohn <jlmohn@ucdavis.edu>
2022-11-28 09:47:56 -0800

    added new bili animals

--- params.gabriel.py
+++ params.gabriel.py
@@ -1,5 +1,39 @@
-## Headfixed
+## Freely Moving
+## FT ##
+FTstage0 = {'experimenter':'jenny', 'outcomeMode':'sides_direct', 'relevantFeature':'spectral', 'delayToTargetMean':0, 'delayToTargetHalfRange':0, 'targetMaxIntensity':60}
 
+FTstage1 = {'experimenter':'jenny', 'outcomeMode':'direct', 'relevantFeature':'spectral', 'delayToTargetMean':0, 'delayToTargetHalfRange':0, 'targetMaxIntensity':60}
+
+FTstage2 = {'experimenter':'jenny', 'outcomeMode':'on_next_correct', 'relevantFeature':'spectral', 'delayToTargetMean':0.01, 'delayToTargetHalfRange':0.0,'automationMode':'increase_delay', 'targetMaxIntensity':60}
+
+FTstage3 = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'spectral', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05, 'targetMaxIntensity':70}
+
+FTstage4 = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'spectral', 'psycurveMode':'extreme80pc', 'psycurveNsteps':'6', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05, 'targetMaxIntensity':70}
+
+FTstage5 = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'spectral', 'psycurveMode':'uniform', 'psycurveNsteps':'6', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05, 'targetMaxIntensity':70}
+
+FTstage6 = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'spectral', 'irrelevantFeatureMode':'random', 'psycurveMode':'uniform', 'psycurveNsteps':'6', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05, 'targetMaxIntensity':70}
+
+
+FTBiasCorr = {'experimenter':'jenny', 'outcomeMode':'only_if_correct', 'relevantFeature':'spectral', 'antibiasMode':'repeat_mistake', 'delayToTargetMean': 0.2, 'delayToTargetHalfRange':0.05, 'targetMaxIntensity':70}
+
+leftBias = {'timeWaterValveL':0.025}
+
+rightBias = {'timeWaterValveR':0.025}
+
+# bili animals
+bili052 = {'subject':'bili052', **FTstage0}
+bili053 = {'subject':'bili053', **FTstage0}
+bili054 = {'subject':'bili054', **FTstage0}
+bili055 = {'subject':'bili055', **FTstage0}
+bili056 = {'subject':'bili056', **FTstage0}
+bili057 = {'subject':'bili057', **FTstage0}
+bili058 = {'subject':'bili058', **FTstage0}
+bili059 = {'subject':'bili059', **FTstage0}
+bili060 = {'subject':'bili060', **FTstage0}
+
+
+## Headfixed
 headfix_habituation = {'rewardSideMode':'toggle'}
 
 febeSpectralStage1 = {'experimenter':'gabriel', 'relevantFeature':'spectral',  'psycurveMode':'off', 'taskMode':'water_after_sound', 'rewardSideMode':'random', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':2.5,'interTrialIntervalHalfRange':1,'targetMaxIntensity':60, 'timeWaterValve':0.03, 'stimType':'sound_only'}
@@ -15,7 +49,6 @@ febeSpectralStage4 = {'experimenter':'gabriel', 'relevantFeature':'spectral',  '
 febeSpectralStage5 = {'experimenter':'gabriel', 'relevantFeature':'spectral',  'psycurveMode':'uniform', 'psycurveNsteps':'6', 'taskMode':'discriminate_stim', 'rewardSideMode':'random', 'lickBeforeStimOffset':'ignore', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':2.5,'interTrialIntervalHalfRange':1,'targetMaxIntensity':60, 'timeWaterValve':0.03, 'stimType':'sound_only'}
 
 febeSpectralStage6 = {'experimenter':'gabriel', 'relevantFeature':'spectral', 'irrelevantFeatureMode':'random', 'psycurveMode':'uniform', 'psycurveNsteps':'6', 'taskMode':'discriminate_stim', 'rewardSideMode':'random', 'lickBeforeStimOffset':'ignore', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':2.5,'interTrialIntervalHalfRange':1,'targetMaxIntensity':60, 'timeWaterValve':0.03, 'stimType':'sound_only'}
-
 
 
 febe013 = {'subject':'febe013', **febeSpectralStage3}
