36209c180b92fbdddfc474f0ab95484dfc392187
Gtoea <91916146+Gtoea@users.noreply.github.com>
2023-03-14 17:46:50 -0700

    Moved bili058 and 060 to bias mode

--- params.gabriel.py
+++ params.gabriel.py
@@ -25,12 +25,12 @@ rightBias = {'timeWaterValveR':0.025}
 bili052 = {'subject':'bili052', **FTstage4}
 bili053 = {'subject':'bili053', **FTstage3}
 bili054 = {'subject':'bili054', **FTstage3}
-bili055 = {'subject':'bili055', **FTBiasCorr}
+bili055 = {'subject':'bili055', **FTstage3}
 bili056 = {'subject':'bili056', **FTstage4}
 bili057 = {'subject':'bili057', **FTstage3}
-bili058 = {'subject':'bili058', **FTstage3}
+bili058 = {'subject':'bili058', **FTBiasCorr}
 bili059 = {'subject':'bili059', **FTstage3}
-bili060 = {'subject':'bili060', **FTstage3}
+bili060 = {'subject':'bili060', **FTBiasCorr}
 
 
 ## Headfixed
