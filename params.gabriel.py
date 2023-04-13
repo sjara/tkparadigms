a3c13f740d367ad2359ac3a706b260211328fac5
Gtoea <91916146+Gtoea@users.noreply.github.com>
2023-04-13 16:16:22 -0700

    Added stage 1- stage 3 of the training paradigm

--- params.gabriel.py
+++ params.gabriel.py
@@ -3,8 +3,33 @@ freqDiscrimStage0 = {'experimenter':'gabriel', 'outcomeMode':'sides_direct', 'de
                      'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
                      'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
                      'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+freqDiscrimStage1 = {'experimenter':'gabriel', 'outcomeMode':'direct', 'delayToTargetMean':0,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+freqDiscrimStage2 = {'experimenter':'gabriel', 'outcomeMode':'on_next_correct', 'delayToTargetMean':0.01,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'automationMode':'increase_delay', 'targetMaxIntensity':60}
+                     'targetMaxIntensity':70,'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+freqDiscrimStage3 = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+freqDiscrimBiasCorr = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct','antibiasMode':'repeat_mistake','delayToTargetMean':0.2,
+                     'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
 
 test000 = {'subject':'test000', **freqDiscrimStage0}
+sole001 = {'subject':'sole001', **freqDiscrimStage0}
+sole002 = {'subject':'sole002', **freqDiscrimStage0}
+sole003 = {'subject':'sole003', **freqDiscrimStage0}
+sole004 = {'subject':'sole004', **freqDiscrimStage0}
+sole005 = {'subject':'sole005', **freqDiscrimStage0}
+sole006 = {'subject':'sole006', **freqDiscrimStage0}
+sole007 = {'subject':'sole007', **freqDiscrimStage0}
+sole008 = {'subject':'sole008', **freqDiscrimStage0}
+sole009 = {'subject':'sole009', **freqDiscrimStage0}
 
 
 
