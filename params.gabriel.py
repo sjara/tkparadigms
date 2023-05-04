2d70e463e1b100c94e9d9d80d26e6b55ca5cc6b0
Gtoea <91916146+Gtoea@users.noreply.github.com>
2023-05-03 21:21:20 -0700

    Moved sole009 to bias mode

--- params.gabriel.py
+++ params.gabriel.py
@@ -21,7 +21,7 @@ freqDiscrimBiasCorr = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct'
                      'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
 
 test000 = {'subject':'test000', **freqDiscrimStage0}
-sole001 = {'subject':'sole001', **freqDiscrimBiasCorr}
+sole001 = {'subject':'sole001', **freqDiscrimStage3}
 sole002 = {'subject':'sole002', **freqDiscrimStage3}
 sole003 = {'subject':'sole003', **freqDiscrimStage3}
 sole004 = {'subject':'sole004', **freqDiscrimStage3}
@@ -29,7 +29,7 @@ sole005 = {'subject':'sole005', **freqDiscrimStage3}
 sole006 = {'subject':'sole006', **freqDiscrimStage3}
 sole007 = {'subject':'sole007', **freqDiscrimBiasCorr}
 sole008 = {'subject':'sole008', **freqDiscrimStage3}
-sole009 = {'subject':'sole009', **freqDiscrimStage3}
+sole009 = {'subject':'sole009', **freqDiscrimBiasCorr}
 
 
 
