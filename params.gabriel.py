75d47a20bdd7281d5d6b5b77ad5342564b03cddc
Gtoea <91916146+Gtoea@users.noreply.github.com>
2023-06-05 17:51:04 -0700

    Update params.gabriel.py

--- params.gabriel.py
+++ params.gabriel.py
@@ -1,15 +1,15 @@
 # === Amplitude Modulation (freely moving) ===
 AMDiscrimStage0 = {'experimenter':'gabriel', 'outcomeMode':'sides_direct', 'delayToTargetMean':0,
                      'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
-                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'targetIntensityMode':'randMinus20',  
                      'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
 AMDiscrimStage1 = {'experimenter':'gabriel', 'outcomeMode':'direct', 'delayToTargetMean':0,
                      'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
-                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'targetIntensityMode':'randMinus20', 
                      'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
 AMDiscrimStage2 = {'experimenter':'gabriel', 'outcomeMode':'on_next_correct', 'delayToTargetMean':0.01,
                      'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'automationMode':'increase_delay', 
-                     'targetMaxIntensity':70,'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'targetMaxIntensity':70,'targetIntensityMode':'randMinus20', 
                      'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
 AMDiscrimStage3 = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2,
                      'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
@@ -17,7 +17,7 @@ AMDiscrimStage3 = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct', 'd
                      'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
 AMDiscrimBiasCorr = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct','antibiasMode':'repeat_mistake','delayToTargetMean':0.2,
                      'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
-                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'targetIntensityMode':'randMinus20', 
                      'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
 
 sole001 = {'subject':'sole001', **AMDiscrimStage3}
