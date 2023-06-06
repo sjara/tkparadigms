e2b6b9e1dde689f2ad003efa8146f100b8221b4d
Gtoea <91916146+Gtoea@users.noreply.github.com>
2023-06-05 17:16:02 -0700

    Update params.gabriel.py

--- params.gabriel.py
+++ params.gabriel.py
@@ -1,3 +1,27 @@
+# === Amplitude Modulation (freely moving) ===
+amDiscrimStage0 = {'experimenter':'gabriel', 'outcomeMode':'sides_direct', 'delayToTargetMean':0,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+amDiscrimStage1 = {'experimenter':'gabriel', 'outcomeMode':'direct', 'delayToTargetMean':0,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+amDiscrimStage2 = {'experimenter':'gabriel', 'outcomeMode':'on_next_correct', 'delayToTargetMean':0.01,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'automationMode':'increase_delay', 
+                     'targetMaxIntensity':70,'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+amDiscrimStage3 = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+amDiscrimBiasCorr = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct','antibiasMode':'repeat_mistake','delayToTargetMean':0.2,
+                     'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
+                     'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
+
+
+
 # === Frequency discrimination (freely moving) ===
 freqDiscrimStage0 = {'experimenter':'gabriel', 'outcomeMode':'sides_direct', 'delayToTargetMean':0,
                      'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
