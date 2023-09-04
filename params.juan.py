70940e0345d98c7f0780e856a6bbc1bd9bbb00b8
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-09-04 12:42:22 -0700

    params 20230905

--- params.juan.py
+++ params.juan.py
@@ -14,3 +14,4 @@ coop018x019 = {'subject':'coop018x019', 'experimenter':'juan', **coopStage4, 'po
 coop022x023 = {'subject':'coop022x023', 'experimenter':'juan', **coopStage4, 'barrierType': 'solid', 'rewardFrequency': 2, 'pokesPerMouse': 5}
 coop024x025 = {'subject':'coop024x025', 'experimenter':'juan', **coopStage4,'barrierType': 'perforated_10_mm','pokesPerMouse': 10,'rewardFrequency': 2 }
 coop026x027 = {'subject':'coop026x027', 'experimenter':'juan', **coopStage4,'barrierType': 'solid', 'pokesPerMouse': 10, 'rewardFrequency': 2} 
+coop028x029 = {'subject':'coop028x029', 'experimenter':'juan', **coopStage1,}
