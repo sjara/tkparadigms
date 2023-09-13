5ddb926300e40e0408c1ea782d4e0c29b7037a83
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-09-13 13:10:20 -0700

    params 20230914

--- params.juan.py
+++ params.juan.py
@@ -13,7 +13,7 @@ coopStage4 = {'taskMode':'cooperate', 'barrierType': 'perforated_10_mm', 'waitTi
 Barrier type:
             ["perforated_5_mm","perforated_10_mm", "solid", "transparent_holes", "transparent_no_holes", "no_barrier"]
 '''
-coop022x023 = {'subject':'coop022x023', 'experimenter':'juan', **coopStage4, 'barrierType': 'solid', 'rewardFrequency': 2, 'pokesPerMouse': 5}
-coop024x025 = {'subject':'coop024x025', 'experimenter':'juan', **coopStage4,'barrierType': 'perforated_10_mm','pokesPerMouse': 10,'rewardFrequency': 2 }
-coop026x027 = {'subject':'coop026x027', 'experimenter':'juan', **coopStage4,'barrierType': 'perforated_10_mm', 'pokesPerMouse': 10, 'rewardFrequency': 2} 
+coop022x023 = {'subject':'coop022x023', 'experimenter':'juan', **coopStage4, 'barrierType': 'perforated_10_mm', 'rewardFrequency': 2, 'pokesPerMouse': 10}
+coop024x025 = {'subject':'coop024x025', 'experimenter':'juan', **coopStage4,'barrierType': 'transparent_no_holes','pokesPerMouse': 10,'rewardFrequency': 2 }
+coop026x027 = {'subject':'coop026x027', 'experimenter':'juan', **coopStage4,'barrierType': 'transparent_no_holes', 'pokesPerMouse': 10, 'rewardFrequency': 2} 
 coop028x029 = {'subject':'coop028x029', 'experimenter':'juan', **coopStage3, "activatePokeIncrement":"False", 'pokesPerMouse': 5, 'targetPokesPerMouse':5}
