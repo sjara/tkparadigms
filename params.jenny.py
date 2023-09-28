5c6f53ff1acd52d19f346670427a1b527ee85525
Jenny Mohn <jlmohn@ucdavis.edu>
2023-09-28 10:45:45 -0700

    updated am behavior params

--- params.jenny.py
+++ params.jenny.py
@@ -11,6 +11,18 @@ tuningAM = {'subject':'feat004','experimenter':'jenny', 'minFreq':4, 'maxFreq':1
             'stimType':'AM', 'stimDur':0.5, 'isiMean':1.2, 'isiHalfRange':0.2, 'minInt':60,
             'maxInt':60, 'syncLightMode':'from_stim_offset', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1}
 
+widefieldLOW = {'subject':'wifi000','experimenter':'jenny', 'minFreq':4000, 'maxFreq':4000,
+              'numTones':1, 'stimType':'Sine', 'stimDur':0.2, 'isiMean':2.5, 'isiHalfRange':0.5,
+              'minInt':70, 'maxInt':70, 'numInt':1, 'syncLightMode':'from_stim_offset', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1 }
+
+widefieldMID = {'subject':'wifi000','experimenter':'jenny', 'minFreq':16000, 'maxFreq':16000,
+              'numTones':1, 'stimType':'Sine', 'stimDur':0.2, 'isiMean':2.5, 'isiHalfRange':0.5,
+              'minInt':70, 'maxInt':70, 'numInt':1, 'syncLightMode':'from_stim_offset', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1 }
+
+widefieldHIGH = {'subject':'wifi000','experimenter':'jenny', 'minFreq':32000, 'maxFreq':32000,
+              'numTones':1, 'stimType':'Sine', 'stimDur':0.2, 'isiMean':2.5, 'isiHalfRange':0.5,
+              'minInt':70, 'maxInt':70, 'numInt':1, 'syncLightMode':'from_stim_offset', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1 }
+
 speechPilot = {'subject':'feat004', 'experimenter':'jenny','outcomeMode':'passive_exposure','delayToTargetMean':3.0,'delayToTargetHalfRange':1,'psycurveMode':'uniform', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1}
 
 FTVOTBorders = {'subject':'feat005', 'experimenter':'jenny','outcomeMode':'passive_exposure','delayToTargetMean':3.0,'delayToTargetHalfRange':1,'psycurveMode':'uniform', 'relevantFeature':'spectral', 'irrelevantFeatureMode':'matrix_border', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1, 'targetMaxIntensity':60}
@@ -27,9 +39,11 @@ AMstage1 = {'experimenter':'jenny', 'soundType':'AM_depth', 'taskMode':'water_af
 
 AMstage2 = {'experimenter':'jenny', 'soundType':'AM_depth', 'taskMode':'lick_on_stim', 'psycurveMode':'off', 'rewardSideMode':'repeat_mistake', 'lickBeforeStimOffset':'ignore', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':2.5,'interTrialIntervalHalfRange':1,'targetIntensity':65, 'timeWaterValve':0.03, 'stimType':'sound_only', 'highAMrate':8, 'lowAMrate':8, 'highAMdepth':100, 'lowAMdepth':0, 'targetDuration':0.3}
 
-AMstage2_5 = {'experimenter':'jenny', 'soundType':'AM_depth', 'taskMode':'lick_on_stim', 'psycurveMode':'off', 'rewardSideMode':'random', 'lickBeforeStimOffset':'abort', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':1,'interTrialIntervalHalfRange':1,'targetIntensity':65, 'timeWaterValve':0.03, 'stimType':'sound_only', 'highAMrate':8, 'lowAMrate':8, 'highAMdepth':100, 'lowAMdepth':0, 'targetDuration':0.3}
+AMstage2b = {'experimenter':'jenny', 'soundType':'AM_depth', 'taskMode':'lick_on_stim', 'psycurveMode':'off', 'rewardSideMode':'random', 'lickBeforeStimOffset':'abort', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':1,'interTrialIntervalHalfRange':1,'targetIntensity':65, 'timeWaterValve':0.03, 'stimType':'sound_only', 'highAMrate':8, 'lowAMrate':8, 'highAMdepth':100, 'lowAMdepth':0, 'targetDuration':0.3}
 
 AMstage3 = {'experimenter':'jenny', 'soundType':'AM_depth', 'taskMode':'discriminate_stim', 'psycurveMode':'off', 'rewardSideMode':'random', 'lickBeforeStimOffset':'abort', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':2.5,'interTrialIntervalHalfRange':1,'targetIntensity':65, 'timeWaterValve':0.03, 'stimType':'sound_only', 'highAMrate':8, 'lowAMrate':8, 'highAMdepth':100, 'lowAMdepth':0, 'targetDuration':0.3}
+
+AMstageBiasCorrect = {'experimenter':'jenny', 'soundType':'AM_depth', 'taskMode':'discriminate_stim', 'psycurveMode':'off', 'rewardSideMode':'repeat_mistake', 'lickBeforeStimOffset':'abort', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':2.5,'interTrialIntervalHalfRange':1,'targetIntensity':65, 'timeWaterValve':0.03, 'stimType':'sound_only', 'highAMrate':8, 'lowAMrate':8, 'highAMdepth':100, 'lowAMdepth':0, 'targetDuration':0.3}
 
 AMstage4 = {'experimenter':'jenny', 'soundType':'AM_depth', 'taskMode':'discriminate_stim', 'psycurveMode':'extreme80pc', 'psycurveNsteps':'6', 'rewardSideMode':'random', 'lickBeforeStimOffset':'abort', 'lickingPeriod':1.5, 'rewardAvailability':1, 'interTrialIntervalMean':2.5,'interTrialIntervalHalfRange':1,'targetIntensity':65, 'timeWaterValve':0.03, 'stimType':'sound_only', 'highAMrate':8, 'lowAMrate':8, 'highAMdepth':100, 'lowAMdepth':0, 'targetDuration':0.3}
 
@@ -38,7 +52,7 @@ AMstage5 = {'experimenter':'jenny', 'soundType':'AM_depth', 'taskMode':'discrimi
 
 ## TEST AM DEPTH ANIMALS ##
 test133 = {'subject':'test133', **AMstage3}
-test134 = {'subject':'test134', **AMstage2_5, 'interTrialIntervalMean':1.7}
+test134 = {'subject':'test134', **AMstageBiasCorrect}
 
 ## VOT ##
 VOTstage0 = {'experimenter':'jenny', 'outcomeMode':'sides_direct', 'relevantFeature':'temporal', 'delayToTargetMean':0, 'delayToTargetHalfRange':0, 'targetMaxIntensity':60}
