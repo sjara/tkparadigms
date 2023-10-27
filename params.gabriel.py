16cee4d7052c11032d5098478b90a861df7f0bcf
Gtoea <91916146+Gtoea@users.noreply.github.com>
2023-10-27 16:44:33 -0700

    Update params.gabriel.py
    Moved sole017 to stage 3

--- params.gabriel.py
+++ params.gabriel.py
@@ -16,12 +16,12 @@ amDiscrimBiasCorr = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct','
                      'targetIntensityMode':'randMinus20',}
 
 sole011 = {'subject':'sole011', **amDiscrimStage3}
-sole012 = {'subject':'sole012', **amDiscrimStage3}
+sole012 = {'subject':'sole012', **amDiscrimBiasCorr}
 sole013 = {'subject':'sole013', **amDiscrimStage3}
 sole014 = {'subject':'sole014', **amDiscrimStage3}
 sole015 = {'subject':'sole015', **amDiscrimStage3}
 sole016 = {'subject':'sole016', **amDiscrimBiasCorr}
-sole017 = {'subject':'sole017', **amDiscrimStage2}
+sole017 = {'subject':'sole017', **amDiscrimStage3}
 
 sole001 = {'subject':'sole001', **amDiscrimStage3}
 sole002 = {'subject':'sole002', **amDiscrimStage3}
