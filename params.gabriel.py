4e2f74f8044e65fa23b5d1bc59032af5c9a20105
Gtoea <91916146+Gtoea@users.noreply.github.com>
2023-11-13 19:56:58 -0800

    Update params.gabriel.py

--- params.gabriel.py
+++ params.gabriel.py
@@ -58,7 +58,7 @@ freqDiscrimBiasCorr = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct'
                      'targetIntensityMode':'randMinus20', 'currentBlock':'mid_boundary', 
                      'highFreq':16200, 'midFreq':9000, 'lowFreq':5000}
 
-sole017 = {'subject':'sole017', **freqDiscrimBiasCorr}
+sole017 = {'subject':'sole017', **freqDiscrimStage3}
 
 test000 = {'subject':'test000', **freqDiscrimStage0}
 #sole001 = {'subject':'sole001', **freqDiscrimStage3}
