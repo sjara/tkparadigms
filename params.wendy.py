99169a959f0d906091822402cfac192878131102
wmiichelle <149523236+wmiichelle@users.noreply.github.com>
2024-01-12 14:26:02 -0800

    Update params.wendy.py

--- params.wendy.py
+++ params.wendy.py
@@ -4,7 +4,7 @@ Parameters for electrophysiological responses of AC neurons to features of speec
 puretones_AC.1sec = {'experimenter':'wendy', 
                      'minFreq':2000, 
                      'maxFreq':40000, 
-                     'numTones': 60, 
+                     'numTones':60, 
                      'MinInt':70, 
                      'maxInt':70, 
                      'numInt':1, 
