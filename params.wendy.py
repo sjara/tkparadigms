d340aa996a6ab73128a327a51a03783a78432ad0
jaratask2 <none>
2024-01-12 14:46:09 -0800

    Parameters for AC neuron recording with pure tones

--- params.wendy.py
+++ params.wendy.py
@@ -1,10 +1,8 @@
-{'experimenter':'wendy', 
+puretones_AC.1sec = {'experimenter':'wendy', 
                      'minFreq':2000, 
                      'maxFreq':40000, 
+                     'numTones':60, 
                      'numTones':60, 
                      'MinInt':70, 
                      'maxInt':70, 
                      'numInt':1, 
-                     'soundLocation':'contralateral', 
-                     'stimDur':0.1, 
-                     'isi':0.5}
