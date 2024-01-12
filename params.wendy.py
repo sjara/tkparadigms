57415068c16aef24a772f2064aebe90c5db1ab8e
wmiichelle <149523236+wmiichelle@users.noreply.github.com>
2024-01-12 14:11:06 -0800

    Update params.wendy.py

--- params.wendy.py
+++ params.wendy.py
@@ -1,4 +1,9 @@
 '''
 Parameters for electrophysiological responses of AC neurons to features of speech (am_tuning.py)
 '''
-puretones_AC.1sec = {'experimenter':'wendy', 'minFreq':2000, 'maxFreq':40000, 'numTones': 60, 'MinInt':70, 'maxInt':70, 'numInt':1, 'soundLocation':'contralateral', 'stimDur':0.1, 'isi':0.5}
+puretones_AC.1sec = {'experimenter':'wendy', 
+                     'minFreq':2000, 'maxFreq':40000, 
+                     'numTones': 60, 'MinInt':70, 
+                     'maxInt':70, 'numInt':1, 
+                     'soundLocation':'contralateral', 
+                     'stimDur':0.1, 'isi':0.5}
