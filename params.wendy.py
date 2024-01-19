2d235b4a12ec7b97e16578fc9b623871cd1b79f1
jaratask2 <none>
2024-01-19 14:47:52 -0800

    parameters for AC and pure tones

--- params.wendy.py
+++ params.wendy.py
@@ -1,7 +1,7 @@
 puretones_AC = {'experimenter':'wendy', 
                      'minFreq':2000, 
                      'maxFreq':40000,
-                     'MinInt':70, 
+                     'minInt':70, 
                      'maxInt':70, 
                      'numInt':1, 
                      'soundLocation':'left', 
