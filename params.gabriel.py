45aadf441ca87c81c972361118659f31adaedf32
Gtoea <91916146+Gtoea@users.noreply.github.com>
2024-01-23 17:24:30 -0800

    Update params.gabriel.py

--- params.gabriel.py
+++ params.gabriel.py
@@ -17,12 +17,12 @@ amDiscrimBiasCorr = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct', 
                      'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
                      'targetIntensityMode':'randMinus20',}
 
-sole021 = {'subject':'sole021', **amDiscrimBiasCorr}
+sole021 = {'subject':'sole021', **amDiscrimStage3}
 sole022 = {'subject':'sole022', **amDiscrimStage3}
 sole023 = {'subject':'sole023', **amDiscrimStage3}
 sole024 = {'subject':'sole024', **amDiscrimStage3}
-sole025 = {'subject':'sole025', **amDiscrimStage3}
-sole026 = {'subject':'sole026', **amDiscrimStage2}
+sole025 = {'subject':'sole025', **amDiscrimBiasCorr}
+sole026 = {'subject':'sole026', **amDiscrimStage3}
 sole027 = {'subject':'sole027', **amDiscrimStage2}
 sole028 = {'subject':'sole028', **amDiscrimStage3}
 sole029 = {'subject':'sole029', **amDiscrimStage3}
