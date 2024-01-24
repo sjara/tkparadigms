0cc76c7ba38250506471ec10b5fcb93244d09716
jaratask2 <none>
2024-01-24 10:21:38 -0800

    updating parameter file

--- params.jacob.py
+++ params.jacob.py
@@ -1,1 +1,1 @@
-puretones_AS = {'numTones':16,'minFreq':2000,'maxFreq':40000,'minInt':70,'maxInt':70,'stimDur':0.1,'isi':0.5,'soundLocation':'binaural','stimType':'Sine'}
+puretones_AS = {'numTones':16,'minFreq':2000,'maxFreq':40000,'minInt':70,'maxInt':70,'stimDur':0.1,'isi':0.5,'soundLocation':'binaural','stimType':'Sine','randomMode':'Random','numInt':1}
