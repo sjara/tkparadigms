29332a2c3839179363955c8d62214e26119ffaed
Gtoea <91916146+Gtoea@users.noreply.github.com>
2024-01-30 20:50:15 -0800

    Update params.gabriel.py

--- params.gabriel.py
+++ params.gabriel.py
@@ -17,15 +17,15 @@ amDiscrimBiasCorr = {'experimenter':'gabriel', 'outcomeMode':'only_if_correct', 
                      'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
                      'targetIntensityMode':'randMinus20',}
 
-sole021 = {'subject':'sole021', **amDiscrimBiasCorr}
-sole022 = {'subject':'sole022', **amDiscrimStage3}
-sole023 = {'subject':'sole023', **amDiscrimBiasCorr}
-sole024 = {'subject':'sole024', **amDiscrimBiasCorr}
+sole021 = {'subject':'sole021', **amDiscrimStage3}
+sole022 = {'subject':'sole022', **amDiscrimBiasCorr}
+sole023 = {'subject':'sole023', **amDiscrimStage3}
+sole024 = {'subject':'sole024', **amDiscrimStage3}
 sole025 = {'subject':'sole025', **amDiscrimStage3}
 sole026 = {'subject':'sole026', **amDiscrimStage3}
-sole027 = {'subject':'sole027', **amDiscrimStage3}
+sole027 = {'subject':'sole027', **amDiscrimBiasCorr}
 sole028 = {'subject':'sole028', **amDiscrimStage3}
-sole029 = {'subject':'sole029', **amDiscrimBiasCorr}
+sole029 = {'subject':'sole029', **amDiscrimStage3}
 
 sole011 = {'subject':'sole011', **amDiscrimStage3}
 sole012 = {'subject':'sole012', **amDiscrimStage3}
