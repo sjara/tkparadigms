b5845f0718f314856c6ac0226ea5aeecdba273e0
jaratask2 <none>
2024-02-07 15:37:39 -0800

    puretones_AM_parameters

--- params.wendy.py
+++ params.wendy.py
@@ -1,11 +1,8 @@
-puretones_AC = {'experimenter':'wendy', 
-'numTones':16,
-'stimType':'Sine',
-                     'minFreq':2000, 
-                     'maxFreq':40000,
-                     'minInt':70, 
-                     'maxInt':70, 
-                     'numInt':1, 
-                     'soundLocation':'left', 
-                     'stimDur':0.1, 
-                     'isi':0.5}
+tuningFreq = {'subject':'feat004','experimenter':'jackie', 'minFreq':2000, 'maxFreq':40000,
+              'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':70, 'maxInt':70, 'numInt':1, 'syncLightMode':'from_stim_offset', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1 }
+
+
+tuningAM = {'subject':'feat004','experimenter':'jackie', 'minFreq':4, 'maxFreq':128, 'numTones':11,
+            'stimType':'AM', 'stimDur':0.5, 'isiMean':1.2, 'isiHalfRange':0.2, 'minInt':60,
+            'maxInt':60, 'syncLightMode':'from_stim_offset', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1}
