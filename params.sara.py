4490bf81f5405af27cd42e9a21c251936b7b589e
srezazadeh98 <srez@uoregon.edu>
2024-12-06 12:39:56 -0800

    Added stages for AM task

--- params.sara.py
+++ params.sara.py
@@ -1,2 +1,22 @@
 # Parameters for paradigms used by Sara
 
+# === Amplitude Modulation (freely moving) ===
+amDiscrimStage0 = {'experimenter':'sara', 'outcomeMode':'sides_direct', 'delayToTargetMean':0,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20',}
+amDiscrimStage1 = {'experimenter':'sara', 'outcomeMode':'direct', 'delayToTargetMean':0,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20',}
+amDiscrimStage2 = {'experimenter':'sara', 'outcomeMode':'on_next_correct', 'delayToTargetMean':0.01,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'automationMode':'increase_delay', 
+                     'targetMaxIntensity':70,'targetIntensityMode':'randMinus20',}
+amDiscrimStage3 = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2,
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20',}
+amDiscrimStage4 = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'psycurveMode':'uniform', 
+                   'psycurveNsteps':'8', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0, 'targetIntensityMode':'randMinus20', 'targetMaxIntensity':70}
+amDiscrimBiasCorr = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'antibiasMode':'repeat_mistake','delayToTargetMean':0.2,
+                     'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
+                     'targetIntensityMode':'randMinus20',}
+
+test000 = {'subject':'sole030', **amDiscrimStage0}
