0d740cc11ec257805a043ed442c6b20e9b823465
srezazadeh98 <srez@uoregon.edu>
2024-12-10 12:34:59 -0800

    Update params.sara.py

--- params.sara.py
+++ params.sara.py
@@ -2,22 +2,29 @@
 
 # === Amplitude Modulation (freely moving) ===
 amDiscrimStage0 = {'experimenter':'sara', 'outcomeMode':'sides_direct', 'delayToTargetMean':0,
-                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
-                     'targetIntensityMode':'randMinus20',}
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':60,
+                     'targetIntensityMode':'fixed',}
 amDiscrimStage1 = {'experimenter':'sara', 'outcomeMode':'direct', 'delayToTargetMean':0,
-                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
-                     'targetIntensityMode':'randMinus20',}
+                     'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':60,
+                     'targetIntensityMode':'fixed',}
 amDiscrimStage2 = {'experimenter':'sara', 'outcomeMode':'on_next_correct', 'delayToTargetMean':0.01,
                      'delayToTargetHalfRange':0, 'allowEarlyWithdrawal':'on', 'automationMode':'increase_delay', 
-                     'targetMaxIntensity':70,'targetIntensityMode':'randMinus20',}
+                     'targetMaxIntensity':60,'targetIntensityMode':'fixed',}
 amDiscrimStage3 = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'delayToTargetMean':0.2,
-                     'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
-                     'targetIntensityMode':'randMinus20',}
+                     'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':60,
+                     'targetIntensityMode':'fixed',}
 amDiscrimStage4 = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'psycurveMode':'uniform', 
-                   'psycurveNsteps':'8', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05, 'targetIntensityMode':'randMinus20', 'targetMaxIntensity':70}
+                   'psycurveNsteps':'8', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05, 'targetIntensityMode':'fixed', 'targetMaxIntensity':60}
 amDiscrimBiasCorr = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'antibiasMode':'repeat_mistake','delayToTargetMean':0.2,
-                     'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':70,
-                     'targetIntensityMode':'randMinus20',}
+                     'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':60,
+                     'targetIntensityMode':'fixed',}
 
 sole039 = {'subject':'sole039', **amDiscrimStage0}
+sole040 = {'subject':'sole040', **amDiscrimStage0}
+sole041 = {'subject':'sole041', **amDiscrimStage0}
+sole042 = {'subject':'sole042', **amDiscrimStage0}
+sole043 = {'subject':'sole043', **amDiscrimStage0}
+sole044 = {'subject':'sole044', **amDiscrimStage0}
+sole045 = {'subject':'sole045', **amDiscrimStage0}
+sole046 = {'subject':'sole046', **amDiscrimStage0}
 sole047 = {'subject':'sole047', **amDiscrimStage0}
