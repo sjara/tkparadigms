8b054e695e1f8b6a3eb4ec999f91e51fc16a4c6b
srezazadeh98 <srez@uoregon.edu>
2025-01-17 14:12:40 -0800

    Update params.sara.py

--- params.sara.py
+++ params.sara.py
@@ -19,12 +19,12 @@ amDiscrimBiasCorr = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'an
                      'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':60,
                      'targetIntensityMode':'fixed',}
 
-sole048 = {'subject':'sole048', **amDiscrimStage2}
+sole048 = {'subject':'sole048', **amDiscrimStage3}
 sole049 = {'subject':'sole049', **amDiscrimStage2}
 sole050 = {'subject':'sole050', **amDiscrimStage1}
 sole051 = {'subject':'sole051', **amDiscrimStage2}
 sole052 = {'subject':'sole052', **amDiscrimStage2}
 sole053 = {'subject':'sole053', **amDiscrimStage2}
-sole054 = {'subject':'sole054', **amDiscrimStage1}
+sole054 = {'subject':'sole054', **amDiscrimStage2}
 sole055 = {'subject':'sole055', **amDiscrimStage2}
-sole056 = {'subject':'sole056', **amDiscrimStage1}
+sole056 = {'subject':'sole056', **amDiscrimStage2}
