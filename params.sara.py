b69ba775121cfc100373347b19e64030299e430e
srezazadeh98 <srez@uoregon.edu>
2025-02-13 15:33:09 -0800

    Update params.sara.py

--- params.sara.py
+++ params.sara.py
@@ -19,15 +19,15 @@ amDiscrimBiasCorr = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'an
                      'delayToTargetHalfRange':0.05, 'allowEarlyWithdrawal':'on', 'targetMaxIntensity':60,
                      'targetIntensityMode':'fixed',}
 
-sole048 = {'subject':'sole048', **amDiscrimStage3}
-sole049 = {'subject':'sole049', **amDiscrimBiasCorr}
-sole050 = {'subject':'sole050', **amDiscrimStage3}
+sole048 = {'subject':'sole048', **amDiscrimBiasCorr}
+sole049 = {'subject':'sole049', **amDiscrimStage3}
+sole050 = {'subject':'sole050', **amDiscrimBiasCorr}
 sole051 = {'subject':'sole051', **amDiscrimStage3}
 sole052 = {'subject':'sole052', **amDiscrimBiasCorr}
 sole053 = {'subject':'sole053', **amDiscrimStage3}
 sole054 = {'subject':'sole054', **amDiscrimStage3}
 sole055 = {'subject':'sole055', **amDiscrimBiasCorr}
-sole056 = {'subject':'sole056', **amDiscrimBiasCorr}
+sole056 = {'subject':'sole056', **amDiscrimStage3}
 sole057 = {'subject':'sole057', **amDiscrimStage0}
 sole058 = {'subject':'sole058', **amDiscrimStage0}
 sole059 = {'subject':'sole059', **amDiscrimStage0}
