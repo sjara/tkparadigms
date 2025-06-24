21125b10c61e50ef0b90e69aae90e9f0c986c429
srezazadeh98 <srez@uoregon.edu>
2025-06-24 11:10:07 -0700

    Update params.sara.py

--- params.sara.py
+++ params.sara.py
@@ -31,9 +31,9 @@ freqDiscrimStage4 = {'experimenter':'sara', 'soundTypeMode': 'tones', 'outcomeMo
 mixedTasks = {'experimenter':'sara', 'soundTypeMode': 'mixed_tones', 'outcomeMode':'only_if_correct', 'psycurveMode':'uniform', 
                    'psycurveNsteps':'6', 'delayToTargetMean':0.2, 'delayToTargetHalfRange':0.05, 'targetIntensityMode':'fixed', 'targetMaxIntensity':60}
 
-sole048 = {'subject':'sole048', **mixedTasks}
+sole048 = {'subject':'sole048', **amDiscrimStage4}
 sole049 = {'subject':'sole049', **amDiscrimStage4}
-sole050 = {'subject':'sole050', **amDiscrimBiasCorr}
+sole050 = {'subject':'sole050', **amDiscrimStage4}
 sole051 = {'subject':'sole051', **mixedTasks}
 sole052 = {'subject':'sole052', **amDiscrimStage4}
 sole053 = {'subject':'sole053', **amDiscrimStage4}
