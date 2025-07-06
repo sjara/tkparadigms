861082507a7e1868b803280cf225f69a3ee1b921
srezazadeh98 <srez@uoregon.edu>
2025-07-06 11:30:18 -0700

    Update params.sara.py

--- params.sara.py
+++ params.sara.py
@@ -36,7 +36,7 @@ mixedTasks = {'experimenter':'sara', 'soundTypeMode': 'mixed_tones', 'outcomeMod
 
 sole048 = {'subject':'sole048', **amDiscrimStage4}
 sole049 = {'subject':'sole049', **amDiscrimStage4}
-sole050 = {'subject':'sole050', **amDiscrimStage4}
+sole050 = {'subject':'sole050', **amDiscrimBiasCorr}
 sole051 = {'subject':'sole051', **amDiscrimStage4}
 sole052 = {'subject':'sole052', **amDiscrimStage4}
 sole053 = {'subject':'sole053', **amDiscrimStage4}
