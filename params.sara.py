0fbc2e24e132c90f205b8ba7f93e14622532585d
srezazadeh98 <srez@uoregon.edu>
2025-08-27 09:15:58 -0700

    Update params.sara.py

--- params.sara.py
+++ params.sara.py
@@ -59,9 +59,9 @@ mixedTasks = {'experimenter':'sara', 'outcomeMode':'only_if_correct', 'delayToTa
               'targetDuration': 0.2, 'psycurveMode':'uniform', 'psycurveNfreq':6, 'soundTypeMode': 'mixed_tones'}
              
 
-sole048 = {'subject':'sole048', **freqDiscrimBiasCorr}
+sole048 = {'subject':'sole048', **freqDiscrimStage3}
 sole049 = {'subject':'sole049', **amDiscrimStage4}
-sole050 = {'subject':'sole050', **freqDiscrimBiasCorr}
+sole050 = {'subject':'sole050', **freqDiscrimStage3}
 sole053 = {'subject':'sole053', **amDiscrimStage4}
 sole056 = {'subject':'sole056', **amDiscrimStage4}
 sole059 = {'subject':'sole059', **freqDiscrimStage3}
@@ -71,11 +71,11 @@ sole067 = {'subject':'sole067', **amDiscrimStage4}
 sole068 = {'subject':'sole068', **amDiscrimStage3}
 sole069 = {'subject':'sole069', **amDiscrimStage3}
 sole070 = {'subject':'sole070', **amDiscrimStage3}
-sole071 = {'subject':'sole071', **amDiscrimBiasCorr}
-sole072 = {'subject':'sole072', **amDiscrimStage03}
-sole073 = {'subject':'sole073', **amDiscrimStage03}
-sole074 = {'subject':'sole074', **amDiscrimStage03}
-sole075 = {'subject':'sole075', **amDiscrimBiasCorr0}
-sole076 = {'subject':'sole076', **amDiscrimBiasCorr0}
-sole077 = {'subject':'sole077', **amDiscrimBiasCorr0}
+sole071 = {'subject':'sole071', **amDiscrimStage3}
+sole072 = {'subject':'sole072', **amDiscrimBiasCorr0}
+sole073 = {'subject':'sole073', **amDiscrimBiasCorr0}
+sole074 = {'subject':'sole074', **amDiscrimBiasCorr0}
+sole075 = {'subject':'sole075', **amDiscrimStage03}
+sole076 = {'subject':'sole076', **amDiscrimStage03}
+sole077 = {'subject':'sole077', **amDiscrimStage03}
 test000 = {'subject':'test000', **amDiscrimStage2}
