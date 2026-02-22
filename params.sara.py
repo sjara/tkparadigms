a6a9502c0e84e6aa808402cd21bd6e4392b65c60
srezazadeh98 <srez@uoregon.edu>
2026-02-22 09:06:17 -0800

    Swap amDiscrimStage03 and amDiscrimBiasCorr0 assignments

--- params.sara.py
+++ params.sara.py
@@ -81,16 +81,16 @@ amDiscrimStage3LaserOn = {'experimenter':'sara', 'outcomeMode':'only_if_correct'
 
 
 
-sole072 = {'subject':'sole072', **amDiscrimStage03}
-sole073 = {'subject':'sole073', **amDiscrimBiasCorr0}
+sole072 = {'subject':'sole072', **amDiscrimBiasCorr0}
+sole073 = {'subject':'sole073', **amDiscrimStage03}
 sole074 = {'subject':'sole074', **amDiscrimStage03}
 sole075 = {'subject':'sole075', **amDiscrimStage03}
-sole076 = {'subject':'sole076', **amDiscrimBiasCorr0}
-sole077 = {'subject':'sole077', **amDiscrimBiasCorr0}
-sole078 = {'subject':'sole078', **amDiscrimBiasCorr0}
-sole079 = {'subject':'sole079', **amDiscrimStage03}
-sole080 = {'subject':'sole080', **amDiscrimStage03}
-sole081 = {'subject':'sole081', **amDiscrimStage03}
+sole076 = {'subject':'sole076', **amDiscrimStage03}
+sole077 = {'subject':'sole077', **amDiscrimStage03}
+sole078 = {'subject':'sole078', **amDiscrimStage03}
+sole079 = {'subject':'sole079', **amDiscrimBiasCorr0}
+sole080 = {'subject':'sole080', **amDiscrimBiasCorr0}
+sole081 = {'subject':'sole081', **amDiscrimBiasCorr0}
 sole082 = {'subject':'sole082', **amDiscrimBiasCorr0}
 sole083 = {'subject':'sole083', **amDiscrimStage3}
 test000 = {'subject':'test000', **amDiscrimStage3}
