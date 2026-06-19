a752b72e4950aee96cd60130a2acf4c6cdf2763b
Santiago Jaramillo <sjara@uoregon.edu>
2026-06-19 14:06:30 -0700

    Move inactive params files to archive/

---
+++ archive/params.george.py
@@ -0,0 +1,13 @@
+
+''' George's params file for recording from implanted animals'''
+
+oddballFMDown= {'experimenter':'george', 'subject':'', 'stimType':'FM', 'oddballStim':'FM_down', 'soundIntensity': 70, 'stimDuration':0.1,
+           'oddballPeriod':10, 'highFreq':13000, 'lowFreq':8000}  
+                    
+oddballFMUp= {'experimenter':'george', 'subject':'', 'stimType':'FM', 'oddballStim':'FM_up', 'soundIntensity': 70, 'stimDuration':0.1,
+           'oddballPeriod':10, 'highFreq':13000, 'lowFreq':8000}
+
+tuningFreq = {'subject':'','experimenter':'george', 'minFreq':2000, 'maxFreq':40000,
+              'numTones':16, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':70, 'maxInt':70, 'numInt':2, 'syncLightMode':'from_stim_offset', 'syncLight':'centerLED', 'delayToSyncLight': 0.2, 'syncLightDuration':0.1 , 'soundLocation':'left'}
+ 
