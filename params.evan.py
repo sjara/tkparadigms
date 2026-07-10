5ee29f28424859fb54c39012d0e47261e3fc8f2e
Evan Vickers <dab22008@gmail.com>
2026-07-10 14:45:24 -0700

    added evan params wf map

---
+++ params.evan.py
@@ -0,0 +1,66 @@
+"""
+This file defines the parameters for taskontrol paradigms.
+"""
+
+lowtoneTrains70 = {'subject':'imag02X', 'experimenter':'Willgaston', 
+              'minFreq':3000, 'maxFreq':3000, 'numTones':1, 
+              'stimType':'ToneTrain', 'stimDur':0.5,
+              'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':70, 'maxInt':70}
+              
+lowtoneTrains80 = {'subject':'imag02X', 'experimenter':'Willgaston', 
+              'minFreq':3000, 'maxFreq':3000, 'numTones':1, 
+              'stimType':'ToneTrain', 'stimDur':0.5,
+              'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':80, 'maxInt':80}
+
+midtoneTrains70 = {'subject':'imag02X', 'experimenter':'Willgaston', 
+              'minFreq':15500, 'maxFreq':15500, 'numTones':1, 
+              'stimType':'ToneTrain', 'stimDur':0.5,
+              'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':70, 'maxInt':70}
+
+midtoneTrains80 = {'subject':'imag02X', 'experimenter':'Willgaston', 
+              'minFreq':15500, 'maxFreq':15500, 'numTones':1, 
+              'stimType':'ToneTrain', 'stimDur':0.5,
+              'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':80, 'maxInt':80}
+
+hightoneTrains70 = {'subject':'imag02X', 'experimenter':'Willgaston', 
+              'minFreq':28000, 'maxFreq':28000, 'numTones':1, 
+              'stimType':'ToneTrain', 'stimDur':0.5,
+              'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':70, 'maxInt':70}
+              
+hightoneTrains80 = {'subject':'imag02X', 'experimenter':'Willgaston', 
+              'minFreq':28000, 'maxFreq':28000, 'numTones':1, 
+              'stimType':'ToneTrain', 'stimDur':0.5,
+              'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':80, 'maxInt':80}
+              
+intensitytoneTrains = {'subject':'imag02X', 'experimenter':'Willgaston', 
+              'minFreq':3000, 'maxFreq':28000, 'numTones':3, 
+              'stimType':'ToneTrain', 'stimDur':0.5,
+              'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':70, 'maxInt':80, 'numInt':2}
+              
+toneTrains = {'subject':'imag02X', 'experimenter':'Willgaston', 
+              'minFreq':3000, 'maxFreq':32000, 'numTones':3, 
+              'stimType':'ToneTrain', 'stimDur':0.5,
+              'isiMean':1.2, 'isiHalfRange':0.2,
+              'minInt':70, 'maxInt':70}
+              
+              
+              
+#toneTrains80LHH = {'subject':'imag02X', 'experimenter':'Willgaston', 
+#              'minFreq':3000, 'maxFreq':32000, 'numTones':3, 
+#              'stimType':'ToneTrain', 'stimDur':0.5,
+#              'isiMean':1.2, 'isiHalfRange':0.2,
+#              'minInt':80, 'maxInt':80}
+
+#AMsounds = {'subject':'test000', 'experimenter':'Lizeth', 
+#              'minFreq':8, 'maxFreq':16, 'numTones':2, 
+#              'stimType':'AM', 'stimDur':1,
+#              'isiMean':1.5, 'isiHalfRange':0.2,
+#              'minInt':65, 'maxInt':65}
+
