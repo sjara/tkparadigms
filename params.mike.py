e50ab2cf3ee1068b13d5b749d6ca66acaf80480f
Evan <evicker2@uoregon.edu>
2026-07-16 13:10:57 -0700

    ok

---
+++ params.mike.py
@@ -0,0 +1,11 @@
+
+tuningFreq = {'subject':'temp','experimenter':'temp', 'numReps':20, 'minFreq':2000, 'maxFreq':4000,
+              'numTones':3, 'stimType':'Sine', 'stimDur':0.05, 'isiMean':.1, 'isiHalfRange':0,
+              'minInt':80, 'maxInt':80, 'numInt':1, 'syncLightMode':'from_stim_offset', 'syncLight':'off', 'delayToSyncLight': 0, 'syncLightDuration':0 }
+
+
+            
+WN = {'subject':'wehrXXXX','experimenter':'sam', 'minFreq':2000, 'maxFreq':32000, 'numTones':1,
+            'stimType':'Noise', 'stimDur':0.1, 'isiMean':2, 'isiHalfRange':0, 'minInt':60,
+            'maxInt':60, 'syncLightMode':'from_stim_offset', 'syncLight':'off', 'delayToSyncLight': 0, 'syncLightDuration':0}
+
---
+++ params.sam.py
@@ -0,0 +1,12 @@
+
+tuningFreq = {'subject':'wehrXXXX','experimenter':'sam', 'minFreq':2000, 'maxFreq':32000, 'numReps': 20,
+              'numTones':17, 'stimType':'Sine', 'stimDur':0.1, 'isiMean':1.5, 'isiHalfRange':0,
+              'minInt':40, 'maxInt':80, 'numInt':3, 'syncLightMode':'from_stim_offset', 'syncLight':'off', 'delayToSyncLight': 0, 'syncLightDuration': 0}
+
+
+            
+WN = {'subject':'wehrXXXX','experimenter':'sam', 'minFreq':2000, 'maxFreq':32000, 'numTones':1,
+            'stimType':'Noise', 'stimDur':0.1, 'isiMean':2, 'isiHalfRange':0, 'minInt':60,
+            'maxInt':60, 'syncLightMode':'from_stim_offset', 'syncLight':'off', 'delayToSyncLight': 0, 'syncLightDuration':0}
+
+mapping = {'subject':'wehrXXXX','experimenter':'sam','minFreq':2000,'maxFreq':32000,'numReps':29,'numTones':5,'stimType':'Sine','stimDur':0.1,'isiMean':1.5,'isiHalfRange':0,'minInt':50,'maxInt':70,'numInt':2,'syncLightMode':'from_stim_offset','syncLight':'off','delayToSyncLight':0,'syncLightDuration':0}
